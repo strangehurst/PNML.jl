7017c52b8b4dcd73c92b808c0ce2b29873d9495f
Jeff Hurst <strangehurst@users.noreply.github.com>
2021-10-06 10:30:07 -0500

    condition forced to number, simplistic implementation

--- src/document.jl
+++ src/document.jl
@@ -10,19 +10,32 @@ end
 
 Document(p) = Document(p[:nets],p[:xml])
 
-"Return nets of 'd' matching the given pntd 'type'."
+"Return nets of `d` matching the given pntd `type`."
 function find_nets end
 find_nets(d::Document, type::AbstractString) = find_nets(d, default_pntd_map[type])
 find_nets(d::Document, type::Symbol) = filter(n->n[:type] === type, d.nets)
+
 first_net(d::Document) = first(d.nets)
 
+
+#= What are the characteristics of a SimpleNet?
+
+Assumptions about labels:
+ place has numeric marking, default 0
+ transition has numeric condition, default 0
+ arc has source, target, numeric inscription value, default 0
+
+=#
+
 """
-SimpleNet wraps the 'place', 'transition' & "arc" collections of a single page.
+SimpleNet wraps the `place`, `transition` & `arc` collections
+of a single page of one net.
 
 Omits the page level of the pnml-defined hierarchy, and
-all labels at the merged net/page level of pnml.
+all labels at the merged net/page level of pnml. Note that
+there may be labels attached to the places, transitions & arcs.
 
-# TODO: Support labels? Some, all, non-standard? 
+# TODO: Support labels at net/page level? Some, all, non-standard? 
 
 A multi-page net can be collpsed by removing referenceTransitions & referencePlaces,
 and merging labels of net and all pages.
@@ -38,30 +51,17 @@ end
 #TODO: Transform Vector{Any} to more specific types. Benchmark first.
 #TODO: Maybe using more wrappers. Starts needing pntd-specific types.
 
-"""
-Given a PnmlDict of a pnml 'net' element, assume there is only one page.
-"""
+SimpleNet(str::AbstractString) = SimpleNet(Document(parse_doc(parsexml(str))))
 SimpleNet(doc::Document) = SimpleNet(first_net(doc))
 SimpleNet(net) = SimpleNet(net[:id], collapse_pages(net))
 SimpleNet(id::Symbol, collapsed) = SimpleNet( id, collapsed[:places], collapsed[:trans], collapsed[:arcs])
-SimpleNet(str::AbstractString) = SimpleNet(Document(parse_doc(parsexml(str))))
 
-#= What are the characteristics of a SimpleNet?
-
-Assumptions about labels:
- place has marking
- transition has condition
- arc has source, target, inscription
-
-marking isa initialMarking, has an integer value representing tokens. Default 0.
-inscription has an integer value. Default 1.
-condition may have a text value. #TODO what to put here?
-=#
 
 """
     collapse_pages(net)
 
 Return NamedTuple holding merged page content.
+
 Start with simplest case of assuming that only the first page is meaningful.
 Collect places, transitions and arcs. #TODO COLLECT LABELS
 """
@@ -75,18 +75,18 @@ places(s::SimpleNet) = s.place
 transitions(s::SimpleNet) = s.transition
 arcs(s::SimpleNet) = s.arc
 
-"Is there any place with 'id' in net 's'?"
+"Is there any place with `id` in net `s`?"
 has_place(s::SimpleNet, id::Symbol)      = any(x -> x[:id] === id, places(s))
 has_transition(s::SimpleNet, id::Symbol) = any(x -> x[:id] === id, transitions(s))
 has_arc(s::SimpleNet, id::Symbol)        = any(x -> x[:id] === id, arcs(s))
 
-"Return the place with 'id' in net 's'."
+"Return the place with `id` in net `s`."
 place(s::SimpleNet, id::Symbol)      = s.place[findfirst(x -> x[:id] === id, places(s))]
 transition(s::SimpleNet, id::Symbol) = s.transition[findfirst(x -> x[:id] === id, transitions(s))]
 arc(s::SimpleNet, id::Symbol)        = s.arc[findfirst(x -> x[:id] === id, arcs(s))]
 
 
-# All pnml nodes have an 'id'.
+# All pnml nodes have an `id`.
 id(node)::Symbol = node[:id]
 
 # Get vector of ids.
@@ -99,22 +99,19 @@ arc_ids(s::SimpleNet) = map(id, arcs(s))
 source(arc)::Symbol = arc[:source]
 target(arc)::Symbol = arc[:target]
 
-"Return vector of arcs that have a source or target of transition 'id'."
+"Return vector of arcs that have a source or target of transition `id`."
 all_arcs(s::SimpleNet, id::Symbol) = filter(a->source(a)===id || target(a)===id, arcs(s))
 
-"Return vector of arcs that have a source of transition 'id'."
+"Return vector of arcs that have a source of transition `id`."
 src_arcs(s::SimpleNet, id::Symbol) = filter(a->source(a)===id, arcs(s))
 
-"Return vector of arcs that have a  target of transition 'id'."
+"Return vector of arcs that have a  target of transition `id`."
 tgt_arcs(s::SimpleNet, id::Symbol) = filter(a->target(a)===id, arcs(s))
 
-"#TODO Return inscription value"
-inscription(a) = a[:inscription][:value]
-    
 
 #TODO  marking, inscription, condition, can be more complicated
 
-"Return marking value of a place."
+"Return marking value of a place `P`."
 function marking(p)::Number
     if !isnothing(p[:marking]) && !isnothing(p[:marking][:value])
         p[:marking][:value]
@@ -122,10 +119,11 @@ function marking(p)::Number
         0
     end
 end
-"Return marking value of place with id 'p'."
+
+"Return marking value of place with id `p`."
 marking(s::SimpleNet, p::Symbol) = marking(place(s,p))
 
-"Return incription value of an arc."
+"Return incription value of `arc`."
 function inscription(arc)::Number
     if !isnothing(arc[:inscription]) && !isnothing(arc[:inscription][:value])
         arc[:inscription][:value]
@@ -133,30 +131,36 @@ function inscription(arc)::Number
         1
     end        
 end
-"Return inscription value of an arc with id 'a'."
+
+"Return inscription value of an arc with id `a`."
 inscription(s::SimpleNet, a::Symbol) = inscription(arc(s,a))
 
 #TODO: Return something more useful in Julia than a string!
-#Return condition value of a transition."
-function condition(transition)::Maybe{String}
-    if !isnothing(transition[:condition]) && !isnothing(transition[:condition][:text])
-        transition[:condition][:text][:content]
+#TODO: Specialize for stochastic pntd.
+"Return condition value of `transition`."
+function condition(transition)::Number
+    if (!isnothing(transition[:condition]) &&
+        !isnothing(transition[:condition][:text]) &&
+        !isnothing(transition[:condition][:text][:content]))
+        #TODO implement full structure handling
+        rate = number_value(transition[:condition][:text][:content])
+        isnothing(rate) ? 0 : rate
     else
-        nothing
+        0
     end
 end
-"Return condition value of a transition with id 't'."
+"Return condition value of a transition with id `t`."
 condition(s::SimpleNet, t::Symbol) = condition(transition(s,t))
 
 """
 Transition function of a Petri Net.
 Each transition has an input vector and an output vector.
-Each vector is indexed by the place on the other end of the arc.
+Each labelled vector is indexed by the place on the other end of the arc.
 Values are inscriptions.
 """
 transition_function(s::SimpleNet) = transition_function(s, transition_ids(s))
-transition_function(s::SimpleNet,v::Vector{Symbol}) =
-    LVector( (;[t=>in_out(s,t) for t in v]...))
+transition_function(s::SimpleNet, v::Vector{Symbol}) =
+    LVector( (; [t=>in_out(s,t) for t in v]...))
 
 """
 Return tuple of input, output labelled vectors with key of place ids and
@@ -174,6 +178,7 @@ initialMarking(s::SimpleNet) = initialMarking(s, place_ids(s))
 initialMarking(s::SimpleNet, v::Vector{Symbol}) =
     LVector( (; [p=>marking(s,p) for p in v]...))
 
+"Return a vector of condition values for net `s`."
 conditions(s::SimpleNet) = conditions(s, transition_ids(s))
 conditions(s::SimpleNet, v::Vector{Symbol}) =
-    LVector( (; [t=>number_value(condition(s,t)) for t in v]...))
+    LVector( (; [t=>condition(s,t) for t in v]...))
