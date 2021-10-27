221c21891a7c9e47dc6b3096d697b6cab8d9bd93
Jeff Hurst <strangehurst@users.noreply.github.com>
2021-10-27 10:03:11 -0500

    move parse_doc, parse_file, parse_str to document.jl

--- src/document.jl
+++ src/document.jl
@@ -7,7 +7,7 @@ struct Document{N,X}
     reg::IDRegistry
 end
 
-Document(p, reg=IDRegistry()) = Document(p[:nets], p[:xml], reg)
+Document(p::PnmlDict, reg=IDRegistry()) = Document(p[:nets], p[:xml], reg)
 Document(s::AbstractString, reg=IDRegistry()) = Document(parse_pnml(root(parsexml(s)); reg), reg)
          
 "Return nets of `d` matching the given pntd `type`."
@@ -21,5 +21,28 @@ first_net(d::Document) = first(d.nets)
 "Return all `nets` of `d`."
 nets(d::Document) = d.nets
   
-#function everypage(f::Function, page...)
-    
+
+"Build pnml from a string."
+function parse_str(str)::PNML.Document
+    doc = EzXML.parsexml(str)
+    parse_doc(doc)
+end
+
+"Build pnml from a file."
+function parse_file(fn)::PNML.Document
+    doc = EzXML.readxml(fn)
+    parse_doc(doc)
+end
+
+""" parse_doc(doc::EzXML.Document)
+
+Return a PNML.Document built from an XML Doncuent.
+A well formed PNML XML document has a single root node: 'pnml'.
+"""
+function parse_doc(doc::EzXML.Document)::PNML.Document
+    reg = PNML.IDRegistry()
+    Document(parse_pnml(root(doc); reg), reg)
+end
+
+
+
