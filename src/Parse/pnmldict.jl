9b103fa8922159dd8919e148a0871d8c9f460539
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-01-30 22:43:04 -0600

    move pid, tag, xmlnode to pnmldict.jl

--- src/Parse/pnmldict.jl
+++ src/Parse/pnmldict.jl
@@ -1,4 +1,55 @@
 """
+Alias for Dict with Symbol key.
+Allows code to have semantic information in type names, better searchability.
+
+$(TYPEDEF)
+"""
+const PnmlDict = Dict{Symbol, Any}
+
+
+"""
+Return pnml id symbol, if argument has one, otherwise return `nothing`.
+
+---
+$(TYPEDSIGNATURES)
+
+$(METHODLIST)
+"""
+function pid end
+pid(::Any) = nothing
+pid(node::PnmlDict)::Symbol = node[:id]
+
+"""
+Return tag symbol, if argument has one, otherwise `nothing`.
+
+---
+$(TYPEDSIGNATURES)
+
+$(METHODLIST)
+"""
+function tag end
+tag(::Any) = nothing
+tag(pdict::PnmlDict)::Symbol = pdict[:tag]
+
+
+"""
+Return xml node field of `d` or `nothing`.
+
+---
+$(TYPEDSIGNATURES)
+
+$(METHODLIST)
+"""
+function xmlnode end
+xmlnode(::Any) = nothing
+xmlnode(pdict::PnmlDict) = pdict[:xml]
+
+
+
+
+
+
+"""
 Return PnmlDict holding contents of a well-formed XML node.
 Expected to be wrapped by a type, not be inside anothe Dict.
 
--- src/types.jl
+++ src/types.jl
@@ -6,47 +6,6 @@ $(TYPEDEF)
 """
 const PnmlDict = Dict{Symbol, Any}
 
-
-"""
-Return pnml id symbol, if argument has one, otherwise return `nothing`.
-
----
-$(TYPEDSIGNATURES)
-
-$(METHODLIST)
-"""
-function pid end
-pid(::Any) = nothing
-pid(node::PnmlDict)::Symbol = node[:id]
-
-"""
-Return tag symbol, if argument has one, otherwise `nothing`.
-
----
-$(TYPEDSIGNATURES)
-
-$(METHODLIST)
-"""
-function tag end
-tag(::Any) = nothing
-tag(pdict::PnmlDict)::Symbol = pdict[:tag]
-
-
-"""
-Return xml node field of `d` or `nothing`.
-
----
-$(TYPEDSIGNATURES)
-
-$(METHODLIST)
-"""
-function xmlnode end
-xmlnode(::Any) = nothing
-xmlnode(pdict::PnmlDict) = pdict[:xml]
-
-
-
-
 """
 Alias for union of type `T` or `nothing`.
 
