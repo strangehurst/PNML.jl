44aa2adc38d1fbcb0a1ac6b4f292fbc1894d05f8
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-05-02 18:12:49 -0500

    add tag(::Structure)

--- src/IR/structure.jl
+++ src/IR/structure.jl
@@ -2,23 +2,23 @@
 $(TYPEDEF)
 $(TYPEDFIELDS)
 
-Used by/in unclaimed high-level pnml labels.  
+Used by/in unclaimed high-level pnml labels.
 Is an abstract syntax tree (ast) expressed in XML.
-Since we do not have knowledge of ast _here_ we place it in a `unclamed_label`
+Note the structural similarity to [`PnmlLabel`](@ref) and [`AnyElement`](@ref)
 
 # Extra 
 There are various defined structure ast variants:
   - Sort of a Place type [builtin, multi, product, user]
   - Term of Place HLMarking  [variable, operator]
   - Term of Transition Condition  [variable, operator]
-  - Term of Arc Inscription [variable, operato`r]
+  - Term of Arc Inscription [variable, operator]
   - Declarations of Declaration * [sort, variable, operator]
 """
 struct Structure{T} #TODO
     tag::Symbol
-    dict::T #TODO AnyElement for bring-up? What should be here?
+    dict::T
     #TODO xml
 end
 Structure(p::Pair{Symbol,PnmlDict}) = Structure(p.first, p.second)
 
-convert(::Type{Maybe{Structure}}, pdict::PnmlDict) = Structure(pdict)
+tag(s::Structure) = s.tag
