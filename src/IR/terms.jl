cd45f6f3e82929357e8ac84a55d6aad484ed4d81
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-05-24 10:17:52 -0500

    Term wraps AbstractDict, functor evaluates to dict[:value] or 1

--- src/IR/terms.jl
+++ src/IR/terms.jl
@@ -16,7 +16,7 @@ Part of the many-sorted algebra attached to nodes on a Petri Net Graph.
   - variable
   - operator
 """
-struct Term{T}  <: AbstractTerm #TODO make mutable?
+struct Term{T<:AbstractDict}  <: AbstractTerm #TODO make mutable?
   tag::Symbol
   dict::T
   #TODO xml
@@ -31,3 +31,9 @@ convert(::Type{Maybe{Term}}, pdict::PnmlDict) = Term(pdict)
 tag(t::Term) = t.tag
 dict(t::Term) = t.dict
 #xml(t::Term) = t.xml
+
+"""
+Evaluate a term by returning the ':value' in `dict`.
+Assumes that `dict` is an `AbstractDictionary`.
+"""
+(t::Term)() = get(t.dict, :value, 1)
