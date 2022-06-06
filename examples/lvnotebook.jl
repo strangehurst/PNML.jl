a24715ee8398cc687318f55cdff9e1ea28122771
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-06-06 10:59:24 -0500

    add notebooks for Petri, Graphs

---
+++ examples/lvnotebook.jl
@@ -0,0 +1,214 @@
+### A Pluto.jl notebook ###
+# v0.19.8
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ a68432ac-b7eb-4985-8232-67bde7bcfd0a
+# ╠═╡ show_logs = false
+begin
+	import Pkg
+    Pkg.activate(mktempdir())	#Pkg.activate("/home/jeff/Jules/PNToyBox")
+	Pkg.add(path="/home/jeff/Jules/PNToyBox/src/PNML")
+	Pkg.add("PlutoUI")
+	Pkg.add("LabelledArrays")
+	Pkg.add("OrdinaryDiffEq")
+	Pkg.add("EzXML")
+	Pkg.add("Plots")
+	Pkg.add("Petri")
+	
+	using PlutoUI
+    using PNML, LabelledArrays, OrdinaryDiffEq
+	using EzXML
+    using Plots
+	using Petri
+end
+
+# ╔═╡ e6e05790-dd20-11ec-39b0-eb0589d76e44
+md"""
+# PNML Lotka-Volterra Notebook
+"""
+
+# ╔═╡ 0143155c-a570-4a03-9cab-5704b7e49968
+
+
+# ╔═╡ cff98b32-c6d1-4c4d-beba-ab46f9a54172
+# ╠═╡ show_logs = false
+TableOfContents(title="TOC", aside=true)
+
+# ╔═╡ 97ed66fd-e8aa-4291-81f2-c4981c521862
+# ╠═╡ show_logs = false
+versioninfo()
+
+# ╔═╡ 9420e502-ed69-443b-944b-9339f2e699b1
+# ╠═╡ show_logs = false
+Pkg.status()
+
+# ╔═╡ 5db3c534-07dc-4a9d-8782-41423da2ccfe
+md"""
+## Scratchpad
+"""
+
+# ╔═╡ a995864c-937e-4a0d-b99a-31fd134e23dc
+let
+	d = PNML.PnmlDict(:labels => PNML.PnmlLabel[])
+	reg = PNML.IDRegistry()
+	pntd = PnmlCore()
+	PNML.add_label!(d,root(EzXML.parsexml("<test1> 1 </test1>")), pntd; reg)
+	PNML.add_label!(d,root(EzXML.parsexml("<test2> 2.0 </test2>")), pntd; reg)
+	PNML.add_label!(d,root(EzXML.parsexml("<test3> true </test3>")), pntd; reg)
+	PNML.add_label!(d,root(EzXML.parsexml("<test4> Pnml.Term() </test4>")), pntd; reg)
+end
+
+# ╔═╡ ddacb450-31cb-4f7d-b62f-c81d0c2df858
+md"""
+## Step 1: Define the states and transitions of the Petri Net
+	 
+Here we have 2 states, wolves and rabbits, and transitions to model predation between the two species in the system.
+"""
+
+# ╔═╡ 22a06164-6096-4a7e-a205-c1686c4b03d8
+xml = """<?xml version="1.0"?>
+    <pnml xmlns="http://www.pnml.org/version-2009/grammar/pnml">
+        <net id="net0" type="nonstandard">
+        <page id="page0">
+            <place id="rabbits"> <initialMarking> <text>100.0</text> </initialMarking> </place>
+            <place id="wolves">  <initialMarking> <text>10.0</text> </initialMarking> </place>
+            <transition id ="birth">     <rate> <text>0.3</text> </rate> </transition>
+            <transition id ="death">     <rate> <text>0.7</text> </rate> </transition>
+            <transition id ="predation"> <rate> <text>0.015</text> </rate> </transition>
+            <arc id="a1" source="rabbits"   target="birth">     <inscription><text>1</text> </inscription> </arc>
+            <arc id="a2" source="birth"     target="rabbits">   <inscription><text>2</text> </inscription> </arc>
+            <arc id="a3" source="wolves"    target="predation"> <inscription><text>1</text> </inscription> </arc>
+            <arc id="a4" source="rabbits"   target="predation"> <inscription><text>1</text> </inscription> </arc>
+            <arc id="a5" source="predation" target="wolves">    <inscription><text>2</text> </inscription> </arc>
+            <arc id="a6" source="wolves"    target="death">     <inscription><text>1</text> </inscription> </arc>
+        </page>
+        </net>
+    </pnml>
+"""
+
+# ╔═╡ 706313c3-946c-46cd-a32d-05a4103ba9a4
+md"""
+## Step 2:Define the parameters and transition rates
+
+Once a model is defined, we can define out initial parameters `u0`, a time
+span `tspan`, and the transition rates of the interactions `β`.
+"""
+
+# ╔═╡ c8bf4d0a-f9a2-4a67-85f1-16ff4e53eea2
+tspan = (0.0,100.0)
+
+# ╔═╡ a3044403-467e-4002-9139-69dfefbf9990
+md"""
+## Step 3: Generate a solver and solve
+Finally we can generate a solver and solve the simulation.
+"""
+
+# ╔═╡ 5979a601-8b07-4f33-a766-f8e3cd421891
+md"""
+birth rate $(@bind BR_slider Slider(0.0:0.01:1.0; show_value=true, default=0.3))
+"""
+
+# ╔═╡ edcda833-758c-4405-9459-0ba6b8c5dd3f
+
+
+# ╔═╡ db25d962-4bb3-44b2-b844-ae4e4b8a9f37
+md"""
+death rate $(@bind DR_slider Slider(0.0:0.01:1.0; show_value=true, default=0.7))
+"""
+
+# ╔═╡ 502fbe5a-8ef5-42d6-84b1-3bf09d8c4998
+md"""
+predatation rate $(@bind PR_slider Slider(0.0:0.001:0.2; show_value=true, default=0.015))
+"""
+
+# ╔═╡ 55da1e63-bdef-4eff-8353-48a2a0e4bb1c
+# Experimental versions on the XML.
+xml2 = """<?xml version="1.0"?>
+<pnml xmlns="http://www.pnml.org/version-2009/grammar/pnml">
+<net id="net0" type="continuous">
+<page id="page0">
+  <place id="rabbits"> <initialMarking>100.0</initialMarking> </place>
+  <place id="wolves">  <initialMarking> <text>10.0</text> </initialMarking> </place>
+  <transition id ="birth">     <rate> $BR_slider </rate> </transition>
+  <transition id ="death">     <rate> $DR_slider </rate> </transition>
+  <transition id ="predation"> <rate> $PR_slider </rate> </transition>
+  <arc id="a1" source="rabbits"   target="birth">     <inscription><text>1</text> </inscription> </arc>
+  <arc id="a2" source="birth"     target="rabbits">   <inscription><text>2</text> </inscription> </arc>
+  <arc id="a3" source="wolves"    target="predation"> <inscription><text>1</text> </inscription> </arc>
+  <arc id="a4" source="rabbits"   target="predation"> <inscription><text>1</text> </inscription> </arc>
+  <arc id="a5" source="predation" target="wolves">    <inscription><text>2</text> </inscription> </arc>
+  <arc id="a6" source="wolves"    target="death">     <inscription><text>1</text> </inscription> </arc>
+</page>
+</net>
+</pnml>
+"""
+
+# ╔═╡ fa79b9c4-188a-4cde-9af3-b956cb0a60cd
+net = PNML.SimpleNet(xml2)
+
+# ╔═╡ a73fda08-7414-4d21-9f0e-0edf5f7cb876
+# ╠═╡ show_logs = false
+lotka = let
+	# **Step 1:** 
+	@show S = PNML.place_ids(net) # [:rabbits, :wolves]
+	@show Δ = PNML.transition_function(net)
+	Petri.Model(S, Δ)
+end;
+
+# ╔═╡ c1dbe979-b219-452f-9f4c-0e4de84d4395
+Graph(lotka)
+
+# ╔═╡ 0c300670-010b-42c9-8f62-f681668050c3
+u0 = PNML.initialMarking(net)
+
+# ╔═╡ 2a1dc647-9aa8-4907-89f3-a15f13b45eb2
+β = PNML.rates(net)
+
+# ╔═╡ 3bc171e2-307d-4868-bd7a-e44857a1131e
+prob = ODEProblem(lotka, u0, tspan, β)
+
+# ╔═╡ 512a12f5-fd88-432b-9a56-880e2bf6879a
+sol = OrdinaryDiffEq.solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8);
+
+# ╔═╡ 91f5cd89-8c4b-4ffb-958e-25dafebbf77d
+plot(sol)
+
+# ╔═╡ Cell order:
+# ╟─e6e05790-dd20-11ec-39b0-eb0589d76e44
+# ╠═0143155c-a570-4a03-9cab-5704b7e49968
+# ╠═a68432ac-b7eb-4985-8232-67bde7bcfd0a
+# ╠═cff98b32-c6d1-4c4d-beba-ab46f9a54172
+# ╠═97ed66fd-e8aa-4291-81f2-c4981c521862
+# ╠═9420e502-ed69-443b-944b-9339f2e699b1
+# ╟─5db3c534-07dc-4a9d-8782-41423da2ccfe
+# ╠═a995864c-937e-4a0d-b99a-31fd134e23dc
+# ╟─ddacb450-31cb-4f7d-b62f-c81d0c2df858
+# ╟─22a06164-6096-4a7e-a205-c1686c4b03d8
+# ╠═55da1e63-bdef-4eff-8353-48a2a0e4bb1c
+# ╠═fa79b9c4-188a-4cde-9af3-b956cb0a60cd
+# ╠═a73fda08-7414-4d21-9f0e-0edf5f7cb876
+# ╠═c1dbe979-b219-452f-9f4c-0e4de84d4395
+# ╟─706313c3-946c-46cd-a32d-05a4103ba9a4
+# ╠═0c300670-010b-42c9-8f62-f681668050c3
+# ╠═2a1dc647-9aa8-4907-89f3-a15f13b45eb2
+# ╠═c8bf4d0a-f9a2-4a67-85f1-16ff4e53eea2
+# ╟─a3044403-467e-4002-9139-69dfefbf9990
+# ╠═3bc171e2-307d-4868-bd7a-e44857a1131e
+# ╠═512a12f5-fd88-432b-9a56-880e2bf6879a
+# ╠═5979a601-8b07-4f33-a766-f8e3cd421891
+# ╠═edcda833-758c-4405-9459-0ba6b8c5dd3f
+# ╠═db25d962-4bb3-44b2-b844-ae4e4b8a9f37
+# ╠═502fbe5a-8ef5-42d6-84b1-3bf09d8c4998
+# ╠═91f5cd89-8c4b-4ffb-958e-25dafebbf77d
---
+++ examples/pngraph.jl
@@ -0,0 +1,183 @@
+### A Pluto.jl notebook ###
+# v0.19.6
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ f4c883b8-e2e9-11ec-28de-addab043979e
+begin
+	import Pkg
+    Pkg.activate(Base.current_project())
+    Pkg.instantiate()
+		
+	using PlutoUI, LabelledArrays, EzXML
+    using Plots, MetaGraphsNext, Graphs, GraphPlot, Compose
+    using PNML: PNML, pid
+end
+
+# ╔═╡ f87a98af-6af6-4a0a-819b-3e472eafe54a
+# ╠═╡ show_logs = false
+let
+	versioninfo()
+	Pkg.status()
+end
+
+# ╔═╡ 41566501-e86b-41af-bd01-2d2f999cc54f
+md"""
+birth rate $(@bind BR_slider NumberField(0.0:0.01:1.0; default=0.3))
+
+death rate $(@bind DR_slider NumberField(0.0:0.01:1.0; default=0.7))
+
+predatation rate $(@bind PR_slider NumberField(0.0:0.001:0.2; default=0.015))
+"""
+
+# ╔═╡ 812bda06-fd40-4b65-b5fa-0aae0c2c8832
+# Experimental versions on the XML.
+xml2 = """<?xml version="1.0"?>
+<pnml xmlns="http://www.pnml.org/version-2009/grammar/pnml">
+<net id="net0" type="continuous">
+<page id="page0">
+  <place id="rabbits"> <initialMarking>100.0</initialMarking> </place>
+  <place id="wolves">  <initialMarking> <text>10.0</text> </initialMarking> </place>
+  <transition id ="birth">     <rate> $BR_slider </rate> </transition>
+  <transition id ="death">     <rate> $DR_slider </rate> </transition>
+  <transition id ="predation"> <rate> $PR_slider </rate> </transition>
+  <arc id="a1" source="rabbits"   target="birth">     <inscription><text>1</text> </inscription> </arc>
+  <arc id="a2" source="birth"     target="rabbits">   <inscription><text>2</text> </inscription> </arc>
+  <arc id="a3" source="wolves"    target="predation"> <inscription><text>1</text> </inscription> </arc>
+  <arc id="a4" source="rabbits"   target="predation"> <inscription><text>1</text> </inscription> </arc>
+  <arc id="a5" source="predation" target="wolves">    <inscription><text>2</text> </inscription> </arc>
+  <arc id="a6" source="wolves"    target="death">     <inscription><text>1</text> </inscription> </arc>
+</page>
+</net>
+</pnml>
+"""
+
+# ╔═╡ 602978fa-4e05-4546-b205-d027880f0ccd
+net = PNML.SimpleNet(xml2)
+
+# ╔═╡ 221681e3-656d-4fa1-8425-cece133df01d
+S = PNML.place_ids(net)
+
+# ╔═╡ ccb4e098-51b1-46dd-b563-1074a7ea9d82
+T = PNML.transition_ids(net)
+
+# ╔═╡ 9f5d18d5-f8b7-44d2-8ddf-8b38be57a0a4
+A = PNML.arc_ids(net)
+
+# ╔═╡ f282f10d-8e6a-462e-be8a-4c072df82d0a
+# LabelledArrays when empty (dimension 0) use Union{} as the element type.
+# Which errors under Pluto and suggests fix of defining this method.
+Base.show(io::IO, ::MIME{Symbol("image/svg+xml")}, ::AbstractVector{Union{}}) = print(io, "∅")
+
+# ╔═╡ 0e9edbf3-df2c-474b-b747-6789dac7a398
+Δ = PNML.transition_function(net)
+
+# ╔═╡ 7d77e7ca-ad32-435a-895e-697dc91c340d
+petri = MetaGraph( DiGraph(); 
+		VertexData = PNML.PnmlNode, #String, # marking, condition
+		EdgeData = PNML.Arc, #Symbol,  # inscription
+		graph_data = "graph_of_pnml")
+
+# ╔═╡ c163301f-964e-4b79-b5f2-39c7bb17d085
+for p in S
+	petri[p] = PNML.place(net, p)
+end
+
+# ╔═╡ 7fc99e91-963a-4ffc-bea3-c54adc652c78
+for t in T
+	petri[t] = PNML.transition(net, t)
+end
+
+# ╔═╡ 596c2657-01ce-4f0a-99d6-10aeae963271
+for a in A
+	arc = PNML.arc(net, a)
+	petri[PNML.source(arc), PNML.target(arc)] = arc
+end
+
+# ╔═╡ a127b05a-fcea-4712-be3c-03f96739de82
+petri
+
+# ╔═╡ 9b6e1948-e841-42b3-b315-e08b136bf047
+PNML.inscription(petri[:birth, :rabbits])
+
+# ╔═╡ c6e10d0d-019d-4fac-98a9-8774dce3af18
+petri[:wolves]
+
+# ╔═╡ c4802576-8425-4e66-aa3d-3fdb405a315e
+petri[:rabbits, :birth]
+
+# ╔═╡ 1ae71749-88eb-4b76-9ba0-901892bce079
+collect(label_for.(Ref(petri),vertices(petri)))
+
+# ╔═╡ f16359db-8e21-42ae-bb4d-eb8a6a75b992
+collect(edges(petri))
+
+# ╔═╡ 2700ad27-4a1e-4410-b9c3-e288059473f9
+# ╠═╡ show_logs = false
+map(edges(petri)) do e
+	@show src(e), dst(e)
+	pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))])
+end
+
+# ╔═╡ 1cfda3d4-e653-4629-b377-cad4ea8ce85a
+[pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))]) for e in edges(petri)]
+
+# ╔═╡ 8ed47098-345d-46d6-aaec-1799ce2f3264
+map(vertices(petri)) do v
+	pid(petri[label_for(petri,v)])
+end
+
+# ╔═╡ ed61c205-1b45-45f2-b6fa-76095e7c898e
+gplot(petri; 
+	nodelabel=collect(label_for.(Ref(petri),vertices(petri))),
+	edgelabel=[pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))]) for e in edges(petri)]
+	)
+
+# ╔═╡ 13b8de0b-84ee-4ba8-abba-0fe353df206c
+md"""
+#### Appendix
+
+Utilities, references, et al.
+"""
+
+# ╔═╡ 5c9c1c4f-c635-4275-a0b3-7b39ca1b8ddf
+
+
+# ╔═╡ Cell order:
+# ╠═f4c883b8-e2e9-11ec-28de-addab043979e
+# ╠═f87a98af-6af6-4a0a-819b-3e472eafe54a
+# ╠═812bda06-fd40-4b65-b5fa-0aae0c2c8832
+# ╠═41566501-e86b-41af-bd01-2d2f999cc54f
+# ╠═602978fa-4e05-4546-b205-d027880f0ccd
+# ╠═221681e3-656d-4fa1-8425-cece133df01d
+# ╠═ccb4e098-51b1-46dd-b563-1074a7ea9d82
+# ╠═9f5d18d5-f8b7-44d2-8ddf-8b38be57a0a4
+# ╟─f282f10d-8e6a-462e-be8a-4c072df82d0a
+# ╠═0e9edbf3-df2c-474b-b747-6789dac7a398
+# ╠═7d77e7ca-ad32-435a-895e-697dc91c340d
+# ╠═c163301f-964e-4b79-b5f2-39c7bb17d085
+# ╠═7fc99e91-963a-4ffc-bea3-c54adc652c78
+# ╠═596c2657-01ce-4f0a-99d6-10aeae963271
+# ╠═a127b05a-fcea-4712-be3c-03f96739de82
+# ╠═9b6e1948-e841-42b3-b315-e08b136bf047
+# ╠═c6e10d0d-019d-4fac-98a9-8774dce3af18
+# ╠═c4802576-8425-4e66-aa3d-3fdb405a315e
+# ╠═1ae71749-88eb-4b76-9ba0-901892bce079
+# ╠═8ed47098-345d-46d6-aaec-1799ce2f3264
+# ╠═f16359db-8e21-42ae-bb4d-eb8a6a75b992
+# ╠═2700ad27-4a1e-4410-b9c3-e288059473f9
+# ╠═1cfda3d4-e653-4629-b377-cad4ea8ce85a
+# ╠═ed61c205-1b45-45f2-b6fa-76095e7c898e
+# ╟─13b8de0b-84ee-4ba8-abba-0fe353df206c
+# ╠═5c9c1c4f-c635-4275-a0b3-7b39ca1b8ddf
