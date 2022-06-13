2089ee29d11d3b060daeed5183bc7b1a849b552b
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-06-13 18:56:31 -0500

    graph example pluto notebook update

--- examples/pngraph.jl
+++ examples/pngraph.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.6
+# v0.19.8
 
 using Markdown
 using InteractiveUtils
@@ -20,7 +20,7 @@ begin
     Pkg.activate(Base.current_project())
     Pkg.instantiate()
 		
-	using PlutoUI, LabelledArrays, EzXML
+	using PlutoUI, LabelledArrays, EzXML, BenchmarkTools
     using Plots, MetaGraphsNext, Graphs, GraphPlot, Compose
     using PNML: PNML, pid
 end
@@ -42,7 +42,6 @@ predatation rate $(@bind PR_slider NumberField(0.0:0.001:0.2; default=0.015))
 """
 
 # ╔═╡ 812bda06-fd40-4b65-b5fa-0aae0c2c8832
-# Experimental versions on the XML.
 xml2 = """<?xml version="1.0"?>
 <pnml xmlns="http://www.pnml.org/version-2009/grammar/pnml">
 <net id="net0" type="continuous">
@@ -120,8 +119,16 @@ petri[:rabbits, :birth]
 # ╔═╡ 1ae71749-88eb-4b76-9ba0-901892bce079
 collect(label_for.(Ref(petri),vertices(petri)))
 
-# ╔═╡ f16359db-8e21-42ae-bb4d-eb8a6a75b992
+# ╔═╡ 8ed47098-345d-46d6-aaec-1799ce2f3264
+map(vertices(petri)) do v
+	pid(petri[label_for(petri,v)])
+end
+
+# ╔═╡ 715e160e-e1cc-4cf8-9f0e-283146abb17f
 collect(edges(petri))
+
+# ╔═╡ f16359db-8e21-42ae-bb4d-eb8a6a75b992
+@benchmark collect(edges(petri))
 
 # ╔═╡ 2700ad27-4a1e-4410-b9c3-e288059473f9
 # ╠═╡ show_logs = false
@@ -130,13 +137,16 @@ map(edges(petri)) do e
 	pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))])
 end
 
+# ╔═╡ 9c706202-e11b-410e-bf73-f3b66cdd9c77
+@benchmark map(edges(petri)) do e
+	pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))])
+end
+
 # ╔═╡ 1cfda3d4-e653-4629-b377-cad4ea8ce85a
 [pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))]) for e in edges(petri)]
 
-# ╔═╡ 8ed47098-345d-46d6-aaec-1799ce2f3264
-map(vertices(petri)) do v
-	pid(petri[label_for(petri,v)])
-end
+# ╔═╡ 5229c597-0941-465b-abbf-fb1f76595bd4
+@benchmark [pid(petri[label_for(petri,src(e)), label_for(petri,dst(e))]) for e in edges(petri)]
 
 # ╔═╡ ed61c205-1b45-45f2-b6fa-76095e7c898e
 gplot(petri; 
@@ -151,14 +161,39 @@ md"""
 Utilities, references, et al.
 """
 
-# ╔═╡ 5c9c1c4f-c635-4275-a0b3-7b39ca1b8ddf
+# ╔═╡ a7990ebd-2d2a-4174-9b4c-173489dd4d83
+html"<hr/>"
+
+# ╔═╡ 22a5da67-b163-4cde-9a8b-9231b0585318
+md"### Analysis"
+
+# ╔═╡ c880b360-30e6-43b4-a484-5d0e6e778233
+md"""
+Incidence Matrix
+
+D = D- - D+- 
+"""
+
+# ╔═╡ 327bb59d-7426-47a3-9cda-01986d6e89e1
+
+
+# ╔═╡ 4c4c48e6-6a26-4a7a-b1b0-343b2a8c75db
+md"S, T Invariantrs"
+
+# ╔═╡ 00125b01-60ef-4602-b7a7-4443a38a665c
+md"Coverability Tree"
+
+# ╔═╡ 3d5a2730-d9ca-4b4c-a193-173403dd6dee
+md"Reachability Graph"
+
+# ╔═╡ 8e740237-eb79-45d1-a974-c026cc827820
 
 
 # ╔═╡ Cell order:
 # ╠═f4c883b8-e2e9-11ec-28de-addab043979e
 # ╠═f87a98af-6af6-4a0a-819b-3e472eafe54a
 # ╠═812bda06-fd40-4b65-b5fa-0aae0c2c8832
-# ╠═41566501-e86b-41af-bd01-2d2f999cc54f
+# ╟─41566501-e86b-41af-bd01-2d2f999cc54f
 # ╠═602978fa-4e05-4546-b205-d027880f0ccd
 # ╠═221681e3-656d-4fa1-8425-cece133df01d
 # ╠═ccb4e098-51b1-46dd-b563-1074a7ea9d82
@@ -175,9 +210,19 @@ Utilities, references, et al.
 # ╠═c4802576-8425-4e66-aa3d-3fdb405a315e
 # ╠═1ae71749-88eb-4b76-9ba0-901892bce079
 # ╠═8ed47098-345d-46d6-aaec-1799ce2f3264
+# ╠═715e160e-e1cc-4cf8-9f0e-283146abb17f
 # ╠═f16359db-8e21-42ae-bb4d-eb8a6a75b992
 # ╠═2700ad27-4a1e-4410-b9c3-e288059473f9
+# ╠═9c706202-e11b-410e-bf73-f3b66cdd9c77
 # ╠═1cfda3d4-e653-4629-b377-cad4ea8ce85a
+# ╠═5229c597-0941-465b-abbf-fb1f76595bd4
 # ╠═ed61c205-1b45-45f2-b6fa-76095e7c898e
 # ╟─13b8de0b-84ee-4ba8-abba-0fe353df206c
-# ╠═5c9c1c4f-c635-4275-a0b3-7b39ca1b8ddf
+# ╟─a7990ebd-2d2a-4174-9b4c-173489dd4d83
+# ╠═22a5da67-b163-4cde-9a8b-9231b0585318
+# ╠═c880b360-30e6-43b4-a484-5d0e6e778233
+# ╠═327bb59d-7426-47a3-9cda-01986d6e89e1
+# ╠═4c4c48e6-6a26-4a7a-b1b0-343b2a8c75db
+# ╠═00125b01-60ef-4602-b7a7-4443a38a665c
+# ╠═3d5a2730-d9ca-4b4c-a193-173403dd6dee
+# ╠═8e740237-eb79-45d1-a974-c026cc827820
