89451bc7aeea420709b36ee31aff31ee7a1a1ff9
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-11-21 16:20:35 -0600

    refactor pnmldict pnmlobject into types

--- src/Core/anyelement.jl
+++ src/Core/anyelement.jl
@@ -17,28 +17,3 @@ AnyElement(p::Pair{Symbol,PnmlDict}, xml::XMLNode) = AnyElement(p.first, p.secon
 tag(a::AnyElement) = a.tag
 dict(a::AnyElement) = a.dict
 xmlnode(a::AnyElement) = a.xml
-
-pid(object::PnmlObject) = object.id
-has_name(o::T) where {T <: PnmlObject} = o.name !== nothing
-name(o::T) where {T <: PnmlObject} = has_name(o) ? o.name.text : ""
-has_labels(o::T) where {T <: PnmlObject} = has_labels(o.com)
-labels(o::T) where {T <: PnmlObject} = labels(o.com)
-
-has_label(o::T, tagvalue::Symbol) where {T <: PnmlObject} =
-    if has_labels(o)
-        l = labels(o)
-        l !== nothing ? has_label(l, tagvalue) : false
-    else
-        false
-    end
-get_label(o::T, tagvalue::Symbol) where {T <: PnmlObject} =
-    if has_labels(o)
-        l = labels(o)
-        l !== nothing ? get_label(l, tagvalue) : nothing
-    else
-        nothing
-    end
-
-has_tools(o::T) where {T <: PnmlObject} = has_tools(o.com) && !isnothing(tools(o.com))
-tools(o::T) where {T <: PnmlObject} = tools(o.com)
-#TODO has_tool, get_tool
--- src/Core/types.jl
+++ src/Core/types.jl
@@ -1,50 +1,95 @@
+# Core types and methods documented in interfaces.jl.
+
 """
 Alias for union of type `T` or `nothing`.
 $(TYPEDEF)
 """
 const Maybe{T} = Union{T, Nothing}
 
+#--------------------------------------------
 """
 $(TYPEDEF)
 Alias for Dict with Symbol as key.
 """
 const PnmlDict = Dict{Symbol, Any}
 
+pid(pdict::PnmlDict)::Symbol = pdict[:id]
+tag(pdict::PnmlDict)::Symbol = pdict[:tag]
+xmlnode(pdict::PnmlDict) = pdict[:xml]
+
+has_labels(pdict::PnmlDict) = haskey(pdict, :labels)
+has_label(d::PnmlDict, tagvalue::Symbol) = if has_labels(d)
+    has_label(labels(d), tagvalue)
+else
+    false
+end
+
+labels(pdict::PnmlDict) = pdict[:labels]
+
+get_label(d::PnmlDict, tagvalue::Symbol) = has_labels(d) ? get_label(labels(d), tagvalue) : nothing
+get_labels(d::PnmlDict, tagvalue::Symbol) = get_labels(labels(d), tagvalue)
+
+#--------------------------------------------
 """
 $(TYPEDEF)
+
 Objects of a Petri Net Graph are pages, arcs, nodes.
 """
 abstract type PnmlObject{PNTD<:PnmlType} end
 
+pid(o::PnmlObject)        = o.id
+tag(o::PnmlObject)        = o.tag
+has_name(o::PnmlObject)   = o.name !== nothing
+name(o::PnmlObject)       = has_name(o) ? o.name.text : ""
+xmlnode(o::PnmlObject)    = has_xml(o) ? o.xml : nothing
+has_labels(o::PnmlObject) = has_labels(o.com)
+labels(o::PnmlObject)     = labels(o.com)
+
+has_label(o::PnmlObject, tagvalue::Symbol) =
+    if has_labels(o)
+        l = labels(o)
+        l !== nothing ? has_label(l, tagvalue) : false
+    else
+        false
+    end
+get_label(o::PnmlObject, tagvalue::Symbol) =
+    if has_labels(o)
+        l = labels(o)
+        l !== nothing ? get_label(l, tagvalue) : nothing
+    else
+        nothing
+    end
+
+has_tools(o::PnmlObject) = has_tools(o.com) && !isnothing(tools(o.com))
+tools(o::PnmlObject)     = tools(o.com)
+#TODO has_tool, get_tool
+
+
+#--------------------------------------------
 """
 $(TYPEDEF)
-Petri Net Graph nodes are places, transitions.
+Petri Net Graph nodes are [`Place`](@ref), [`Transition`](@ref).
+They are the source or target of an [`Arc`](@ref)
 """
 abstract type PnmlNode{PNTD} <: PnmlObject{PNTD} end
 
 """
 $(TYPEDEF)
-For common behavior shared by [`RefPlace`](@ref), [`RefTransition`](@ref).
+For common behavior shared by [`RefPlace`](@ref), [`RefTransition`](@ref)
+used to connect [`Page`](@ref) together.
 """
 abstract type ReferenceNode{PNTD} <: PnmlNode{PNTD} end
 
+"Return the `id` of the referenced node."
+refid(r::ReferenceNode) = r.ref
+
+#--------------------------------------------
 """
 $(TYPEDEF)
 
-Tool specific objects can be attached to `PnmlObject`s and `AbstractLabel`s subtypes.
+Tool specific objects can be attached to
+[`PnmlObject`](@ref)s and [`AbstractLabel`](@ref)s subtypes.
 """
 abstract type AbstractPnmlTool end #TODO see ToolInfo
 
-
-"""
-$(TYPEDEF)
-Labels are attached to the Petri Net Graph object subtypes. See [`PnmlObject`](@ref).
-"""
-abstract type AbstractLabel end
-
-"""
-$(TYPEDEF)
-Label that may be displayed.
-Differs from an Attribute Label by possibly having a [`Graphics`](@ref) field.
-"""
-abstract type Annotation <: AbstractLabel end
+#--------------------------------------------
