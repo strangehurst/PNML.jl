cb9337e04d67a4202bd97bf0a0ad6bdd2a2f2e17
Jeff Hurst <strangehurst@users.noreply.github.com>
2022-12-08 16:54:56 -0600

    add markingvaluetype, inscriptionvaluetype

--- src/utils.jl
+++ src/utils.jl
@@ -16,3 +16,18 @@ Use PNML type as trait to select type of inscription.
 """
 inscriptiontype(::PnmlType) = PTInscription
 inscriptiontype(::AbstractHLCore) = HLInscription
+
+
+"""
+Use PNML type as trait to select valuetype of marking.
+"""
+markingvaluetype(::PnmlType) = Int
+markingvaluetype(::AbstractContinuousNet) = Float64
+markingvaluetype(::AbstractHLCore) = Term
+
+"""
+Use PNML type as trait to select type of inscription.
+"""
+inscriptionvaluetype(::PnmlType) = Int
+inscriptionvaluetype(::AbstractContinuousNet) = Float64
+inscriptionvaluetype(::AbstractHLCore) = Term
