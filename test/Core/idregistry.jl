f7afa96fe86b0e917a93d1e4a667cc735ccaeee6
Jeff Hurst <strangehurst@users.noreply.github.com>
2023-01-19 13:52:54 -0600

    improve tests

--- test/Core/idregistry.jl
+++ test/Core/idregistry.jl
@@ -1,25 +1,17 @@
 using PNML, EzXML, ..TestUtils, JET
 #using PNML: Maybe
 using .PnmlIDRegistrys
-using .PnmlIDRegistrys: duplicate_id_warn,duplicate_id_error, ,duplicate_id_none, reset_registry!
+using .PnmlIDRegistrys: duplicate_id_warn, duplicate_id_error, duplicate_id_none, reset_registry!
 using .PnmlIDRegistrys: PnmlIDRegistry as IDRegistry
 
-@testset "ID registry" begin
-    reg = IDRegistry()
+@testset "ID registry" for action in (duplicate_id_warn,duplicate_id_error,duplicate_id_none)
 
     #@test_opt IDRegistry()
     #@test_opt target_modules=(@__MODULE__,) register_id!(reg, :p)
-    #@test_opt target_modules=(@__MODULE__,) register_id!(reg, "p")
-    #@test_opt target_modules=(@__MODULE__,) reset_registry!(reg)
-    #@test_opt target_modules=(@__MODULE__,) duplicate_id_action(:p; action=:bogus)
 
-    @test_call IDRegistry()
-    @test_call register_id!(reg, :p)
-    @test_call register_id!(reg, "p")
-    @test_call reset_registry!(reg)
-    @test_call duplicate_id_warn(:p)
-    @test_call duplicate_id_error(:p)
-    @test_call duplicate_id_none(:p)
+    action = duplicate_id_warn
+    @show reg = IDRegistry(; duplicate=action)
+    reset_registry!(reg)
 
     register_id!(reg, "p")
     @test @inferred isregistered_id(reg, "p")
@@ -27,9 +19,20 @@ using .PnmlIDRegistrys: PnmlIDRegistry as IDRegistry
     reset_registry!(reg)
     @test !isregistered_id(reg, "p")
     @test !isregistered_id(reg, :p)
+    register_id!(reg, :p)
+    register_id!(reg, "q")
+end
 
-    @test_logs (:warn,"ID already registered: p") duplicate_id_action(:p)
-    @test_logs (:warn,"ID already registered: p") duplicate_id_action(:p; action=:warn)
-    @test_throws ArgumentError duplicate_id_action(:p; action=:error)
-    @test @inferred( duplicate_id_action(:p; action=:bogus) ) === nothing
+@testset "test_call"  for action in (duplicate_id_warn,duplicate_id_error,duplicate_id_none)
+    @test_call IDRegistry(; duplicate=action)
+    reg = IDRegistry(; duplicate=action)
+    @test_call register_id!(reg, :p)
+    @test_call register_id!(reg, "p")
+    @test_call reset_registry!(reg)
+end
+
+@testset "duplicate actions" begin
+    @test_logs (:warn,"ID already registered: p") duplicate_id_warn(:p)
+    @test_throws ArgumentError duplicate_id_error(:p)
+    @test @inferred( duplicate_id_none(:p) ) === nothing
 end
