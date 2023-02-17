a135b28d1d619ef3e2901ef3291c481e34286f4b
Jeff Hurst <strangehurst@users.noreply.github.com>
2023-02-17 15:05:25 -0600

    use FunctionWrapper on _harvest_any

--- src/Parse/anyelement.jl
+++ src/Parse/anyelement.jl
@@ -25,13 +25,20 @@ nested dictionary holding the contents of the label.
 function unclaimed_label(node::XMLNode, pntd::PnmlType, reg)::Pair{Symbol,PnmlDict}
     @show nodename(node)
     #val = _harvest_any!(node, pntd, _harvest_any!, reg)
-    val = _harvest_any!(node, pntd, _harvest_any!, reg)
+    ha = HarvestAny(_harvest_any, pntd, reg)
+    val = ha(node,)
     @show val
     return Symbol(nodename(node)) => val
 end
 
 struct HarvestAny
-    fun::FunctionWrapper{PnmlDict, Tuple(XMLNode, PnmlType, HarvestAny, PnmlIDRegistry)}
+    fun::FunctionWrapper{PnmlDict, Tuple{XMLNode, HarvestAny}}
+    pntd::PnmlType
+    reg::PnmlIDRegistry
+end
+
+(ha::HarvestAny)(node::XMLNode) = ha.fun(node, ha)
+
 """
 $(TYPEDSIGNATURES)
 
@@ -47,19 +54,19 @@ Note the assumption that "children" and "content" are mutually exclusive.
 Content is always a leaf element. However XML attributes can be anywhere in
 the hierarchy. And neither children nor content nor attribute may be present.
 """
-function _harvest_any!(node::XMLNode, pntd::PnmlType, parser::F, reg)::PnmlDict where {F}
+function _harvest_any(node::XMLNode, ha::HarvestAny)::PnmlDict
     # Extract XML attributes. Register IDs as symbols.
     dict = PnmlDict()
     for a in eachattribute(node)
         # ID attributes can appear in various places. Each is unique and added to the registry.
-        dict[Symbol(a.name)] = a.name == "id" ? register_id!(reg, a.content) : a.content
+        dict[Symbol(a.name)] = a.name == "id" ? register_id!(ha.reg, a.content) : a.content
     end
 
     # Extract children or content
     children = elements(node)
     @show length(children)
     if !isempty(children)
-        merge!(dict, _anyelement_content(children, pntd, parser, reg))
+        merge!(dict, _anyelement_content(children, ha))
     elseif !isempty(nodecontent(node))
         # <tag> </tag> will have nodecontent, though the whitespace is discarded.
         dict[:content] = strip(nodecontent(node))
@@ -77,9 +84,7 @@ Apply `parser` to each node in `nodes`.
 Return PnmlDict with values that are vectors when there
 are multiple instances of a tag in `nodes` and scalar otherwise.
 """
-function _anyelement_content(nodes::Vector{XMLNode},
-                             pntd::PnmlType,
-                             parser::F, reg)::PnmlDict where {F<:Function}
+function _anyelement_content(nodes::Vector{XMLNode}, ha::HarvestAny)::PnmlDict
     namevec = [nodename(node) => node for node in nodes if node !== nothing] # Not yet Symbols.
     tagnames = unique(map(first, namevec))
     @show map(first, namevec)
@@ -88,9 +93,9 @@ function _anyelement_content(nodes::Vector{XMLNode},
     for tagname in tagnames
         tags = filter(x -> x.first === tagname, namevec)
         dict[Symbol(tagname)] = if length(tags) > 1 # Now its a symbol.
-            parser.(map(x -> x.second, tags), Ref(pntd), Ref(parser), Ref(reg)) # vector
+            ha.(map(x -> x.second, tags)) # vector
         else
-            parser(tags[1].second, pntd, parser, reg) # scalar
+            ha(tags[1].second) # scalar
         end
     end
     return dict
