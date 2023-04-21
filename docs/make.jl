5c5958654dc3603c3d8a8edb7bbf10a1d33e5c49
Jeff Hurst <strangehurst@users.noreply.github.com>
2023-04-21 13:56:47 -0500

    devbranch = monorepo

--- docs/make.jl
+++ docs/make.jl
@@ -112,7 +112,8 @@ makedocs(;
 if !isempty(get(ENV, "DOCUMENTER_KEY", ""))
     deploydocs(;
                repo="github.com/strangehurst/PNML.jl",
-               devbranch = "main",
+               devbranch = "monorepo",
+               #devbranch = "main",
                push_preview = true,
                )
 end
