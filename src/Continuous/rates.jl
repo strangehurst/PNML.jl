0cbb2d6c4f7fa028020a977e83daaa71e8c3d98c
Jeff Hurst <strangehurst@users.noreply.github.com>
2023-05-21 16:58:24 -0500

    refactor rate

--- src/Continuous/rates.jl
+++ src/Continuous/rates.jl
@@ -24,25 +24,16 @@ Return rate value of `transition`.  Missing rate labels are defaulted to 0.0.
 function rate(transition)
     # Expected XML:
     # <rate> <text>0.3</text> </rate>
+    #println("rate"); dump(transition)
     ishighlevel(nettype(transition)) &&
         throw(ArgumentError("The `rate` label is not supported for High-Level Petri Nets." *
                             "  Recommended to use a `ContinuousNet`."))
     pntd = nettype(transition)
     R = rate_value_type(pntd)
-    r = get_label(transition, :rate)
-    #@show pntd R r
-    return isnothing(r) ? zero(R) : numeric_label_value(R, r)
-    # if !isnothing(r)
-    #     if haskey(r.elements, :text)
-    #         @assert !isnothing(r.elements[:text])
-    #         # The unclaimed label mechanism adds a :content key for text elements.
-    #         value = number_value(rate_value_type(pntd), r.elements[:text][1][:content])
-    #     elseif haskey(r.elements, :content)
-    #         # When the text element is elided, there is still a :content.
-    #         value = number_value(rate_value_type(pntd), r.elements[:content])
-    #     else
-    #         throw(ArgumentError("`rate` tag missing a value"))
-    #     end
-    #     return value
-    # end
+    if has_label(transition, :rate)
+        r = get_label(transition, :rate)
+        #@show r
+        return numeric_label_value(R, r)
+    end
+    return zero(R)
 end
