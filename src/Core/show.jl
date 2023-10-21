abf7160ff299e8d6094ad8146987dbbb87a513a2
Jeff Hurst <strangehurst@users.noreply.github.com>
2023-10-21 09:58:35 -0500

    remove unused code

--- src/Core/show.jl
+++ src/Core/show.jl
@@ -45,21 +45,6 @@ function Base.show(io::IO, c::Coordinate)
     print(io, "(", c.x, ",", c.y, ")")
 end
 
-function Base.show(io::IO, ::MIME"text/plain", c::Coordinate)
-    print(io, c)
-end
-
-#-------------------
-function shownames(io::IO, g::Graphics)
-    print(io, "Graphics(",
-        "dimension=", g.dimension,
-        ", fill=",      g.fill,
-        ", font=",      g.font,
-        ", line=",      g.line,
-        ", offset=",    g.offset,
-        ", positions=",  g.positions, ")")
-end
-
 function Base.show(io::IO, g::Graphics)
     print(io, "Graphics(",
             g.dimension, ", ",
@@ -90,44 +75,15 @@ function Base.show(io::IO, label::PnmlLabel)
     pprint(io, label)
 end
 
-function Base.show(io::IO, mime::MIME"text/plain", label::PnmlLabel)
-    pprint(io, label)
-end
-
 PrettyPrinting.quoteof(l::PnmlLabel) = :(PnmlLabel($(PrettyPrinting.quoteof(l.tag)),
                                                    $(PrettyPrinting.quoteof(l.elements))))
 
 #-------------------
-# function Base.show(io::IO, elvector::Vector{AnyElement})
-#     show(io, MIME"text/plain"(), elvector)
-# end
-# function Base.show(io::IO, mime::MIME"text/plain", elvector::Vector{AnyElement})
-#     print(io, typeof(elvector), "[")
-#     io = inc_indent(io)
-#     for (i,el) in enumerate(elvector)
-#         print(io, "\n", indent(io), "$i: ")
-#         pprint(io, el)
-#     end
-#     print(io, "]")
-# end
-
-function Base.show(io::IO, el::AnyElement) #TODO Make parametric.
-    pprint(io, el)
-end
-
-function Base.show(io::IO, mime::MIME"text/plain", el::AnyElement) #TODO Make parametric.
-    pprint(io, el)
-end
 
 PrettyPrinting.quoteof(a::AnyElement) = :(AnyElement($(PrettyPrinting.quoteof(a.tag)),
                                                      $(PrettyPrinting.quoteof(a.elements))))
 
-#-------------------
-Base.summary(io::IO, ti::ToolInfo) = print(io, summary(ti))
-function Base.summary(ti::ToolInfo)
-    string(typeof(ti), " name ", ti.toolname, ", version ", ti.version,
-           ", ", length(ti.infos), " infos")
-end
+# #-------------------
 
 function Base.show(io::IO, toolvector::Vector{ToolInfo})
     for (i, ti) in enumerate(toolvector)
@@ -144,29 +100,8 @@ PrettyPrinting.quoteof(ti::ToolInfo) = :(ToolInfo($(PrettyPrinting.quoteof(ti.to
                                          $(PrettyPrinting.quoteof(ti.version)),
                                          $(PrettyPrinting.quoteof(ti.infos))))
 
-#-------------------
 function Base.show(io::IO, tg::TokenGraphics)
     print(io, "positions: ", tg.positions)
-end
-
-function Base.show(io::IO, ::MIME"text/plain", tg::TokenGraphics)
-    print(io, tg)
-end
-
-#-------------------
-function Base.show(io::IO, name::Name)
-    print(io, typeof(name), " '", name.text, "'")
-    isnothing(name.graphics) || print(io, ", has graphics")
-    isempty(name.tools)    || print(io, ", ", length(name.tools), " tool info")
-end
-function Base.show(io::IO, ::MIME"text/plain", name::Name)
-    print(io, name)
-end
-
-#---------------------------------------------------------------------------------
-Base.summary(io::IO, place::Place)  = summary(io, summary(place))
-function Base.summary(place::Place)
-    string(typeof(place))
 end
 
 function Base.show(io::IO, place::Place)
@@ -177,63 +112,16 @@ function Base.show(io::IO, place::Place)
           ", initial marking ", initial_marking(place))
 end
 
-function Base.show(io::IO, ::MIME"text/plain", place::Place)
-    show(io, place)
-end
-function Base.show(io::IO, ::MIME"text/plain", placevector::Vector{Place})
-    show(io, placevector)
-end
-
-function Base.show(io::IO, placevector::Vector{Place})
-    isempty(placevector) && return
-    for (i,place) in enumerate(placevector)
-        print(io, indent(io), place)
-        i < length(placevector) && print(io, "\n")
-    end
-end
-
-#-------------------
 function Base.show(io::IO, trans::Transition)
     print(io, typeof(trans),
           " id ", pid(trans), ", name '", name(trans), "'", ", condition ", condition(trans))
 end
 
-function Base.show(io::IO, ::MIME"text/plain", transvector::Vector{Transition})
-    show(io, transvector)
-end
-
-function Base.show(io::IO, transvector::Vector{Transition})
-    for (i,trans) in enumerate(transvector)
-        print(io, indent(io), trans)
-        i < length(transvector) && print(io, "\n")
-    end
-end
-
-function Base.show(io::IO, ::MIME"text/plain", trans::Transition)
-    show(io, trans)
-end
-
-#-------------------
-#TODO Make RefPlace, RefTransition an Abstract Type
-function Base.show(io::IO, ::MIME"text/plain", r::ReferenceNode)
-    show(io, r)
-end
 function Base.show(io::IO, r::ReferenceNode)
     print(io, typeof(r), " (id ", pid(r), ", ref ", refid(r))
     print(io, ")")
 end
 
-function Base.show(io::IO, ::MIME"text/plain", rvector::Vector{<:ReferenceNode})
-    show(io, rvector)
-end
-function Base.show(io::IO, rvector::Vector{<:ReferenceNode})
-    for (i,r) in enumerate(rvector)
-        print(io, indent(io), r)
-        i < length(rvector) && print(io, "\n")
-    end
-end
-
-#-------------------
 function Base.show(io::IO, arc::Arc)
     print(io, typeof(arc), " id ", arc.id,
           ", name '", has_name(arc) ? name(arc) : "", "'",
@@ -241,18 +129,7 @@ function Base.show(io::IO, arc::Arc)
           ", target: ", target(arc),
           ", inscription: ", arc.inscription)
 end
-function Base.show(io::IO, ::MIME"text/plain", arcvector::Vector{Arc})
-    for (i,arc) in enumerate(arcvector)
-        print(io, indent(io), arc)
-        i < length(arcvector) && print(io, "\n")
-    end
-end
-function Base.show(io::IO, ::MIME"text/plain", arc::Arc)
-    show(io, arc)
-end
 
-#-------------------
-Base.summary(io::IO, page::Page) = print(io, summary(page))
 function Base.summary( page::Page)
     string(typeof(page)," id ", page.id, ", ",
            " name '", name(page), "', ",
@@ -277,9 +154,6 @@ function show_page_field(io::IO, label::AbstractString, x)
     end
 end
 
-function Base.show(io::IO, ::MIME"text/plain", p::Page)
-    show(io, p)
-end
 function Base.show(io::IO, page::Page)
     #TODO Add support for :trim and :compact
     println(io, indent(io), summary(page))
@@ -295,19 +169,7 @@ function Base.show(io::IO, page::Page)
     show_page_field(inc_io, "subpages:",       page_idset(page))
 end
 
-function Base.show(io::IO, ::MIME"text/plain", pagevec::Vector{Page})
-    show(io, pagevec)
-end
-function Base.show(io::IO, pagevec::Vector{Page})
-    isempty(pagevec) && return
-    for (i,page) in enumerate(pagevec)
-        show(io, MIME"text/plain"(), page)
-        i < length(pagevec) && println(io)#, "\n")
-    end
-end
-
-#-------------------
-Base.summary(io::IO, net::PnmlNet) = print(io, summary(net))
+# #-------------------
 function Base.summary(net::PnmlNet)
     string( typeof(net), " id ", pid(net),
             " name '", has_name(net) ? name(net) : "", "', ",
@@ -336,7 +198,6 @@ function Base.show(io::IO, ::MIME"text/plain", net::PnmlNet)
 end
 
 #-------------------
-Base.summary(io::IO, pnml::PnmlModel) = print(io, summary(pnml))
 function Base.summary(pnml::PnmlModel)
     string(typeof(pnml), " model with ",  length(nets(pnml)), " nets" )
 end
@@ -353,19 +214,9 @@ function Base.show(io::IO, pnml::PnmlModel)
     end
 end
 
-function Base.show(io::IO, ::MIME"text/plain", pnml::PnmlModel)
-    return show(io, pnml)
-end
-
 
 #-------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------
-
-#-------------------
-Base.summary(io::IO, ptm::Marking)  = summary(io, summary(ptm))
-function Base.summary(ptm::Marking)
-    string(typeof(ptm))
-end
 
 function Base.show(io::IO, ptm::Marking)
     pprint(io, ptm)
@@ -401,8 +252,6 @@ PrettyPrinting.quoteof(i::Inscription) = :(Inscription($(PrettyPrinting.quoteof(
                                                     $(PrettyPrinting.quoteof(i.tools))
                                             ))
 
-                                            #-------------------
-Base.summary(io::IO, hlm::HLMarking) = summary(hlm)
 function Base.summary(hlm::HLMarking)
     string(typeof(hlm))
 end
@@ -426,54 +275,15 @@ quoteof(i::HLInscription) =
                     $(quoteof(graphics(i))), $(quoteof(tools(i)))))
 
 #-------------------
-function Base.show(io::IO, declarations::Vector{AbstractDeclaration})
-    iio = inc_indent(io)
-    print(io, indent(io), typeof(declarations), "[")
-
-    for (i,dec) in enumerate(declarations)
-        print(iio, "\n", indent(iio))
-        show(inc_indent(io), MIME"text/plain"(), dec)
-    end
-    print(io, "]")
-end
 function Base.show(io::IO, declare::AbstractDeclaration)
     pprint(io, declare)
 end
+
 quoteof(i::AbstractDeclaration) = :(AbstractDeclaration($(quoteof(i.id)), $(quoteof(i.name))))
-#         $(quoteof(i.sort))))
-#-------------------
-function Base.show(io::IO, terms::Vector{AbstractTerm})
-    iio = inc_indent(io)
-    print(io, indent(io), typeof(terms), "[")
 
-    for (i,term) in enumerate(terms)
-        print(iio, "\n", indent(iio))
-        show(inc_indent(io), term)
-    end
-    print(io, "]")
-end
-function Base.show(io::IO, term::AbstractTerm)
-    pprint(io, term)
-end
-quoteof(t::AbstractTerm) = :(AbstractTerm($(quoteof(t.tag)), $(quoteof(t.elements))))
-
-function Base.show(io::IO, term::Term)
-    pprint(io, term)
-end
 quoteof(t::Term) = :(Term($(quoteof(t.tag)), $(quoteof(t.elements))))
 
 #-------------------
-function Base.show(io::IO, nsorts::Vector{NamedSort})
-
-    print(io, typeof(nsorts), "[")
-    for (i,dec) in enumerate(nsorts)
-        print(io, "\n", indent(io))
-        show(inc_indent(io), dec)
-        i < length(nsorts) && print(io, "\n")
-    end
-    print(io, "]")
-end
-
 function Base.show(io::IO, nsort::NamedSort)
     pprint(IOContext(io, :displaysize => (24, 180)), nsort)
 end
