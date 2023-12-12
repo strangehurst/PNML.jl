f65a061075b18b07eef3053b0bf1e3ab427cc6b7
Jeff Hurst <strangehurst@users.noreply.github.com>
2023-12-12 12:11:25 -0600

    interrupt only a single file, more time info printed

--- snoopy/all_pnml.jl
+++ snoopy/all_pnml.jl
@@ -11,7 +11,6 @@ pnml_files(files) = filter(files) do f
     isfile(f) && success(run(Cmd(`grep -qF "<pnml" $f`, ignorestatus=true)))
 end
 
-
 function testpn(dir::AbstractString = "examples";
                 topdir = "/home/jeff/PetriNet/PNML",
                 outdir = "/home/jeff/Jules/testpmnl")
@@ -30,10 +29,11 @@ function testpn(dirs = ("examples",);
     filelogger = FileLogger(outputlog)
     demux_logger = TeeLogger(consolelogger, MinLevelLogger(filelogger, Logging.Debug))
     global_logger(demux_logger)
-    
+
     @show topdir dirs outdir outputlog
     df = DataFrame()
-    
+    start_time = now()
+    @info "start time" start_time
     @time for srcdir in dirs #! Loop over input directories
         in_dir  = joinpath(topdir, srcdir)
         cd(in_dir) do
@@ -41,7 +41,7 @@ function testpn(dirs = ("examples",);
             for (root, dirs, files) in walkdir(".")
                 #map(println, Iterators.map(f -> joinpath(root, f), dirs))
                 flist = map(f -> joinpath(root, f), filter(endswith(r"pnml|xml"), files))
-                map(println, flist)
+                #map(println, flist)
                 pnmls = filter(f -> success(run( Cmd(`grep -qF "<pnml xmlns" "$f"`, ignorestatus=true))), flist)
                 for file in pnmls
                     per_file!(df, outdir, file)
@@ -50,7 +50,7 @@ function testpn(dirs = ("examples",);
             println()
         end
     end
-    
+    @show files_done = now()
     # Exception Summary Report
     xfile = open(joinpath(outdir, "exceptions.txt"), "w")
     cd(outdir) do
@@ -63,25 +63,27 @@ function testpn(dirs = ("examples",);
         end
     end # cd
     close(xfile)
-    
+
     sort!(df, [:time])
     write(joinpath(outdir, "DataFrame.txt"), repr(df))
     CSV.write(joinpath(outdir, "DataFrame.csv"), df)
+    println("finish_time = ", now(), ", elapsed time = ", (start_time - now()))
 end
 
 #-----------------------------------
+# `filename` is relative to the cwd. `outdir` is absolute or relative
 function per_file!(df, outdir::AbstractString, filename::AbstractString)
     outfile = joinpath(outdir, string(filename, ".txt")) #(first ∘ splitext)(filename))
     #@show filename
 
     isfile(outfile) && println("Warning overwriting $outfile")
     mkpath(dirname(outfile)) # Create output directory.
-
-    @info "File $filename size = $(filesize(filename))" # Display path to file and size.
+    file_start = now()
+    @info "File $filename at $(Time(file_start)) size = $(filesize(filename))" # Display path to file and size.
 
     Base.redirect_stdio(stdout=outfile, stderr=outfile) do
         try
-            println(stat(filename))
+            println(stat(filename), " at ", Time(file_start))
             println()
             stats = @timed parse_file(filename)
             push!(df, (file=filename, fsize=filesize(filename),
@@ -89,7 +91,7 @@ function per_file!(df, outdir::AbstractString, filename::AbstractString)
 
             # Display PnmlModel as a test of parsing, creation and show().
             println(stats.value)
-            println()
+            println("took ", stats.time, " memory bytes :", stats.bytes)
 
             # Petri Net & Graph
             @showtime anet = PNML.SimpleNet(stats.value)
@@ -111,9 +113,10 @@ function per_file!(df, outdir::AbstractString, filename::AbstractString)
 
             println("\n\nCAUGHT EXCEPTION:", sprint(showerror, e, bt)) # full backtrace to file
             @info "CAUGHT EXCEPTION: $(sprint(showerror,e))"
-            if e isa InterruptException
-                rethrow()
-            end
+            #! Ignore first ^C, it serves to end prossing of a single file.
+            #! The "second" (is there a window of opurtunity?) should end the loop processing files.
+            # e isa InterruptException && rethrow()
+            # end
         end # try
     end   # redirect
     # print exceptions
