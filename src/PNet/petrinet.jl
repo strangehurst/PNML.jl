9c5cc7a3301b9212951214fc79fa42c534ac5d26
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-01-25 08:06:48 -0600

    add SimpleNet(::XMLNode) constructror

--- src/PNet/petrinet.jl
+++ src/PNet/petrinet.jl
@@ -306,6 +306,7 @@ struct SimpleNet{PNTD} <: AbstractPetriNet{PNTD}
 end
 
 SimpleNet(s::AbstractString) = SimpleNet(parse_str(s))
+SimpleNet(node::XMLNode) = SimpleNet(parse_pnml(node, registry(CONFIG.lock_registry ? ReentrantLock() : nothing)))
 function SimpleNet(model::PnmlModel)
     net0 = first_net(model)
     SimpleNet(net0)
