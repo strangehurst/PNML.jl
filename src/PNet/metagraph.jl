f3deed694287b99949832a85e6083b185fca8204
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-01-28 16:40:15 -0600

    metagraph for PnmlNet

--- src/PNet/metagraph.jl
+++ src/PNet/metagraph.jl
@@ -13,28 +13,33 @@ using MetaGraphsNext: MetaGraph
 
 """
     metagraph(::AbstractPetriNet) -> MetaGraph
+    metagraph(::PnmlNet) -> MetaGraph
 
 Return MetaGraph instance based on a `SimpleDiGraph` with PNML nodes attached to vertices and edges.
 """
-function metagraph(pn::AbstractPetriNet)
+function metagraph end
+
+metagraph(pn::AbstractPetriNet) = metagraph(pnmlnet(pn))
+
+function metagraph(net::PnmlNet)
     # map pnml id symbol to vertex code.
-    vc = vertex_codes(pnmlnet(pn)) # inverse is vertex_labels(pnmlnet(pn))
+    vc = vertex_codes(net) # inverse is vertex_labels(net)
 
     # Create a directed graph from every arc in the petri net graph.
-    graph = SimpleDiGraphFromIterator(Edge(vc[source(a)] => vc[target(a)]) for a in arcs(pn))
+    graph = SimpleDiGraphFromIterator(Edge(vc[source(a)] => vc[target(a)]) for a in arcs(net))
 
     # Map id to (vertex code, label).
     vertexdata = Dict{Symbol, Tuple{Int, Union{Place, Transition}}}()
-    for p in places(pn)
+    for p in places(net)
         vertexdata[pid(p)] = (vc[pid(p)], p)
     end
-    for t in transitions(pn)
+    for t in transitions(net)
         vertexdata[pid(t)] = (vc[pid(t)], t)
     end
 
     # Map from (src,dst) to arc. Uses pid, not vertex codes of graph.
-    edgedata = Dict((source(a), target(a)) => a for a in arcs(pn))
+    edgedata = Dict((source(a), target(a)) => a for a in arcs(net))
 
-    MetaGraph(graph, vertex_labels(pnmlnet(pn)), vertexdata, edgedata, PNML.name(pn),
+    MetaGraph(graph, vertex_labels(net), vertexdata, edgedata, PNML.name(net),
                 edge_data -> 1.0, 1.0) # weights
 end
