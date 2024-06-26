9ab97a6c0ab87e2ed3598103a6241423459afa92
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-06-26 10:50:55 -0500

    refactor auxilliary projects

--- experiments/src/pnmltuples.jl
+++ experiments/src/pnmltuples.jl
@@ -10,10 +10,6 @@ struct PnmlTuple{sorts, T} where {sorts, T <: Tuple}
     tup::T
 end
 
-# if nameof(@__MODULE__) === :Base
-
-
-# PnmlTuple(tuple(AbstractSort), tuple(AbstractTerm)
 @eval function (PT::Type{PnmlTuple{sorts,T}})(args::Tuple) where {sorts, T <: Tuple}
     if length(args) != length(sorts::Tuple)
         throw(ArgumentError("Wrong number of arguments to pnml tuple constructor."))
