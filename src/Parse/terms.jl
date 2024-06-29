c3b8f0d6a6183ac9869dda70ba8f88803f3f5290
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-06-29 13:52:11 -0500

    show term,sort pairs of interms, insorts

--- src/Parse/terms.jl
+++ src/Parse/terms.jl
@@ -62,7 +62,10 @@ function parse_operator_term(tag::Symbol, node::XMLNode, pntd::PnmlType; ids::Tu
         push!(insorts, s) #~ sort may be inferred from place, variable, operator output
     end
     @assert length(interms) == length(insorts)
-    @show interms insorts
+    for (t,s) in zip(interms,insorts)
+        @show t s
+        println()
+    end
     #^ What can we assume from ids?
     #^ Is last(ids) different for partition, partition element, FEC, EnumSort
     outsort = pnml_hl_outsort(tag; insorts, ids) #! some sorts need content
