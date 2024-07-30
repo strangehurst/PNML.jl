cbd715efbfe8ce2d8e3052dd86513c8e20b34bb5
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-07-29 22:29:07 -0500

    StringSort is a singleton

--- src/sorts/strings.jl
+++ src/sorts/strings.jl
@@ -1,17 +1,7 @@
 """
 $(TYPEDEF)
-"""
-@auto_hash_equals struct StringSort <: AbstractSort
-    ae::Vector{AbstractSort}
-end
-StringSort() = StringSort(IntegerSort[])
 
-function Base.show(io::IO, s::StringSort)
-    print(io, "StringSort([")
-    io = inc_indent(io)
-    for  (i, c) in enumerate(s.ae)
-        print(io, '\n', indent(io)); show(io, c);
-        i < length(s.ae) && print(io, ",")
-    end
-    print(io, "])")
-end
+"""
+@auto_hash_equals struct StringSort <: AbstractSort end
+Base.eltype(::Type{<:StringSort}) = String
+sortelements(::StringSort) = tuple("")
