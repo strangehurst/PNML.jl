6af2ed71277fe0b959388d8e83f70abcc786a646
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-09-12 09:08:30 -0500

    split tests, add !select, failfast=false

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,47 +17,49 @@ using .TestUtils
 "Return true if `ARGS` is empty or one of `v` is found in `ARGS`."
 select(v...) = isempty(ARGS) || any(∈(ARGS), v)
 
-if select("none", "NONE")
-    return nothing # Have chosen to bail before any tests.
-end
-
 #############################################################################
 @time "TESTS" begin
 
-@testset verbose=true failfast=true showtiming=true "PNML.jl" begin
+@testset verbose=true failfast=false showtiming=true "PNML.jl" begin
+    if select("none", "NONE")
+        return nothing # Have chosen to bail before any tests.
+    end
     if select("ALL", "AQUA")
         @testset "Aqua" begin
             Aqua.test_all(PNML;
-              ambiguities=(recursive=false),
-              #unbound_args=true,
-              #undefined_exports=true,
-              #project_extras=true,
-              stale_deps=(ignore=[:Metatheory],),
-              deps_compat=(ignore=[:Metatheory],),
-              #project_toml_formatting=true,
+                ambiguities=(recursive=false),
+                #unbound_args=true,
+                #undefined_exports=true,
+                #project_extras=true,
+                #stale_deps=(ignore=[:Metatheory],),
+                #deps_compat=(ignore=[:Metatheory],),
+                #project_toml_formatting=true,
 
-              piracies=false,
-              persistent_tasks=false, # Metatheory ale/3.0 is not in registry
+                piracies=false,
+                #persistent_tasks=false, # Metatheory ale/3.0 is not in registry
             )
-          end
+        end
     end
-    if select("ALL", "BASE")
+    if select("ALL", "BASE") && !select("!BASE")
         println("BASE")
         @safetestset "typedefs"  begin include("typedefs.jl") end
         @safetestset "registry"  begin include("idregistry.jl") end
         @safetestset "utils"     begin include("utils.jl") end
     end
-    if select("ALL", "REWRITE")
+    if select("ALL", "REWRITE") && !select("!REWRITE")
         println("REWRITE")
         @safetestset "rewrite"     begin include("rewrite.jl") end
     end
-    if select("ALL", "CORE")
+    if select("ALL", "CORE") && !select("!CORE")
         println("CORE")
         @safetestset "graphics"     begin include("graphics.jl") end
         @safetestset "toolspecific" begin include("toolspecific.jl") end
         @safetestset "labels"       begin include("labels.jl") end
     end
-    if select("ALL", "CORE2")
+    if select("ALL", "HL") && !select("!HL")
+        @safetestset "labels_hl"       begin include("labels_hl.jl") end
+    end
+    if select("ALL", "CORE2") && !select("!CORE2")
         @safetestset "declarations" begin include("declarations.jl") end
         @safetestset "nodes"        begin include("nodes.jl") end
         @safetestset "pages"        begin include("pages.jl") end
@@ -65,18 +67,18 @@ end
         @safetestset "flatten"      begin include("flatten.jl") end
     end
 
-    if select("ALL2", "NET")
+    if select("ALL1", "NET") && !select("!NET")
         println("NET")
         @safetestset "document"     begin include("document.jl") end
         @safetestset "parse_tree"   begin include("parse_tree.jl") end
     end
 
-     if select("ALL2", "NET2")
+     if select("ALL2", "NET2") && !select("!NET2")
         @safetestset "rate"         begin include("rate.jl") end
         @safetestset "simplenet"    begin include("simplenet.jl") end
     end
 
-    if select("ALL", "DOC")
+    if select("ALL", "DOC") && !select("!DOC")
         println("DOC")
         @testset "doctest" begin doctest(PNML, manual = true) end
     end
