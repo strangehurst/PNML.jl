2b5f8a4a8cd82255b619caf579d33222fbb3d073
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-10-23 08:29:46 -0500

    type parameter subtypes Number

--- src/Core/graphics.jl
+++ src/Core/graphics.jl
@@ -39,7 +39,7 @@ Coordinate(x::T1, y::T2) where {T1 <: Number, T2 <: Number} =
 coordinate_type(::Type{T}) where {T <: PnmlType} = Coordinate{coordinate_value_type(T)}
 coordinate_value_type() = Float32
 coordinate_value_type(::Type) = Float32
-Base.eltype(::Coordinate{T}) where {T} = T
+Base.eltype(::Coordinate{T}) where {T<:Number} = T
 x(c::Coordinate) = c.x_
 y(c::Coordinate) = c.y_
 Base.:(==)(l::Coordinate, r::Coordinate) = x(l) == x(r) && y(l) == y(r)
