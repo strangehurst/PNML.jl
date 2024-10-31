2e04f2312d1a6900a4db85178526ca6b687c7c45
Jeff Hurst <strangehurst@users.noreply.github.com>
2024-10-31 11:00:05 -0500

    remove type parameter, use Symbol/REFID  instead

--- src/declarations/arbitrarydeclarations.jl
+++ src/declarations/arbitrarydeclarations.jl
@@ -3,13 +3,16 @@
 $(TYPEDEF)
 $(TYPEDFIELDS)
 
-Arbitrary sorts that can be used for constructing terms are
-reserved for/supported by `HLPNG` in the pnml specification.
+Arbitrary sorts that can be used for constructing terms are reserved for/supported by `HLPNG` in the pnml specification.
+
+> ...arbitrary sorts and operators do not come with a definition of the sort or operation; they just introduce a new symbol.
+
+Like `ArbitraryOperator`, does not have an associated algebra, not usable by `SymmetricNet.`
 """
-struct ArbitrarySort{S} <: SortDeclaration
+struct ArbitrarySort <: SortDeclaration
     id::Symbol # TODO NamedSort?
     name::Union{String,SubString{String}}
-    body::S
+    body::Symbol #! Are id and declaration redundent?
 end
 
 function ArbitrarySort()
@@ -20,11 +23,12 @@ end
 $(TYPEDEF)
 $(TYPEDFIELDS)
 
-> ...arbitrary sorts and operators do not come with a definition of the sort or operation; they just introduce a new symbol
+> ...arbitrary sorts and operators do not come with a definition of the sort or operation; they just introduce a new symbol.
+
+Like `ArbitrarySort`, does not have an associated algebra, not usable by `SymmetricNet.`
 """
-struct ArbitraryOperator{O} <: OperatorDeclaration
-    declaration::Symbol
-    input::Vector{UserSort} # Sorts
-    output::UserSort # sort of operator
-    body::O # implementation of opearator
+struct ArbitraryOperator <: OperatorDeclaration
+    id::Symbol
+    name::Union{String,SubString{String}}
+    declaration::Symbol #! Are id and declaration redundent?
 end
