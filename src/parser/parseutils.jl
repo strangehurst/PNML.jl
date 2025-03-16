fc45e8d1347a5f682ccd2cad1384783ccb03e7be
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-03-16 18:22:20 -0500

    Add docstrings

--- src/parser/parseutils.jl
+++ src/parser/parseutils.jl
@@ -1,12 +1,27 @@
 #---------------------------------------------------------------------
 # LABELS
 #--------------------------------------------------------------------
+#! Extension point. user supplied parser -> Annotation.
+# Could do conversion from unparsed_tag.
+#! 2 collections, one for PnmlLabels other for other Annotations?
 
+"""
+    add_label!(collection, node, pntd) -> collection
+
+Parse and add [`PnmlLabel`](@ref) to collection, return collection.
+
+See [`AbstractPnmlObject`](@ref) for those XML entities that have labels.
+Any "unknown" XML is presumed to be a label.
+"""
 function add_label!(v::Vector{PnmlLabel}, node::XMLNode, pntd)
-    #! Extension point. user supplied parser of DictType -> Annotation. Could do conversion after/on demand.
-    #! 2 collections, one for PnmlLabels other for other Annotations?
     return push!(v, PnmlLabel(unparsed_tag(node)...))
 end
+
+"""
+    add_label(infos::Maybe{collection}, node::XMLNode, pntd) -> collection
+
+Allocate storage for collection on first use. Then parse and add a label.
+"""
 function add_label(v::Maybe{Vector{PnmlLabel}}, node::XMLNode, pntd)
     labels = isnothing(v) ? PnmlLabel[] : v
     return add_label!(labels, node, pntd)
@@ -15,19 +30,23 @@ end
 # TOOLINFO
 #---------------------------------------------------------------------
 """
-$(TYPEDSIGNATURES)
+    add_toolinfo!(collection, node, pntd) -> collection
 
-Add [`ToolInfo`](@ref) to `infos`, return nothing.
+Parse and add [`ToolInfo`](@ref) to `infos` collection, return `infos`.
 
 The UML from the _pnml primer_ (and schemas) use <toolspecific>
 as the tag name for instances of the type ToolInfo.
 """
 function add_toolinfo!(infos::Vector{ToolInfo}, node, pntd)
-    push!(infos, parse_toolspecific(node, pntd))
-    return infos
+    return push!(infos, parse_toolspecific(node, pntd))
 end
 
-add_toolinfo(infos::Maybe{Vector{ToolInfo}}, node::XMLNode, pntd) = begin
+"""
+    add_toolinfo(infos::Maybe{collection}, node::XMLNode, pntd) -> collection
+
+Allocate storage for `infos` on first use. Then add to `infos`.
+"""
+function add_toolinfo(infos::Maybe{Vector{ToolInfo}}, node::XMLNode, pntd)
     i = isnothing(infos) ? ToolInfo[] : infos
     return add_toolinfo!(i, node, pntd)
 end
