ff3633b84f4cff907592b40550c102b279e3d7da
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-03-27 18:14:34 -0500

    BooleanConstant() refactor

--- src/terms/booleans.jl
+++ src/terms/booleans.jl
@@ -1,16 +1,17 @@
 # boolean sort operators
 
 """
-    BooleanConstant("true"|"false")
+    BooleanConstant("true"|"false") is a built-in operator (constants are 0-ary operators).
+    c = BooleanConstant(true); c() == true
 """
 struct BooleanConstant <: AbstractOperator
     value::Bool
 end
 
 function BooleanConstant(s::Union{AbstractString,SubString{String}})
-    s == "true" || s == "false" || throw(ArgumentError("BooleanConstant unexpected value $s"))
-    BooleanConstant(parse(eltype(BoolSort), s))
+    BooleanConstant(parse(eltype(sortdefinition(namedsort(:bool))), s))
 end
+
 tag(::BooleanConstant) = :booleanconstant
 sortref(::BooleanConstant) = usersort(:bool)::UserSort # usersort,namedsort duo
 sortof(::BooleanConstant) = sortdefinition(namedsort(:bool)) # usersort,namedsort duo
--- test/rewrite.jl
+++ test/rewrite.jl
@@ -1,11 +1,14 @@
 using PNML, ..TestUtils, JET, Metatheory
 
 println("REWRITE")
-@show d = DotConstant()
-@show Metatheory.rewrite(d, PNML.dot)
+@with PNML.idregistry => PnmlIDRegistry() PNML.DECLDICT => PNML.DeclDict() begin
+    PNML.fill_nonhl!()
+    @show d = DotConstant()
+    @show Metatheory.rewrite(d, PNML.dot)
 
-@show btrue = PNML.BooleanConstant("true")
-@show bfalse = PNML.BooleanConstant("false")
+    @show btrue = PNML.BooleanConstant("true")
+    @show bfalse = PNML.BooleanConstant("false")
 
-@show Metatheory.rewrite(btrue, PNML.bool_alg)
-@show Metatheory.rewrite(bfalse, PNML.bool_alg)
+    @show Metatheory.rewrite(btrue, PNML.bool_alg)
+    @show Metatheory.rewrite(bfalse, PNML.bool_alg)
+end
