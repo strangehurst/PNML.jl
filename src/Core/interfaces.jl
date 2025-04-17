d82da9e7eb32233d1fafce9183cc50f688574dd5
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-04-17 18:08:07 -0500

    remove def_sort_elements

--- src/Core/interfaces.jl
+++ src/Core/interfaces.jl
@@ -430,17 +430,7 @@ function sortref end
 
 function sortelements end
 
-"""
-    def_sort_element(x)
 
-Return an arbitrary element of sort `x`.
-All sorts are expected to be iteratable and non-empty, so we return `first`.
-Uses include default inscription value and default initial marking value sorts.
-
-`x` can be anything with a `sortelements(x)` method that returns an iterator with length.
-See [`AbstractSort`](@ref), [`SortType`](@ref).
-"""
-function def_sort_element end
 
 function usersort end
 
