47d2a74dc2349f1fceaca2a6f68b2b74881ad2c8
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-05-03 17:34:23 -0500

    add NUPN  & WoPeD toolspecific tests

--- test/toolspecific.jl
+++ test/toolspecific.jl
@@ -2,30 +2,26 @@ using PNML, ..TestUtils, JET
 
 using OrderedCollections
 
-str1 = (tool="JARP", version="1.2",
-        str = """
- <toolspecific tool="JARP" version="1.2">
+str1 = (tool="JARP", version="1.2", str = """
+<toolspecific tool="JARP" version="1.2">
     <FrameColor><value>java.awt.Color[r=0,g=0,b=0]</value></FrameColor>
     <FillColor><value>java.awt.Color[r=255,g=255,b=255]</value></FillColor>
- </toolspecific>
+</toolspecific>
 """)
 
-str2 = (tool="de.uni-freiburg.telematik.editor", version="1.0",
-        str = """
- <toolspecific tool="de.uni-freiburg.telematik.editor" version="1.0">
+str2 = (tool="de.uni-freiburg.telematik.editor", version="1.0", str = """
+<toolspecific tool="de.uni-freiburg.telematik.editor" version="1.0">
     <visible>true</visible>
- </toolspecific>
+</toolspecific>
 """)
 
-str3 = (tool="petrinet3", version="1.0",
-        str = """
- <toolspecific tool="petrinet3" version="1.0">
+str3 = (tool="petrinet3", version="1.0", str = """
+<toolspecific tool="petrinet3" version="1.0">
     <placeCapacity capacity="0"/>
- </toolspecific>
+</toolspecific>
 """)
 
-str4 = (tool="org.pnml.tool", version="1.0",
-        str = """
+str4 = (tool="org.pnml.tool", version="1.0", str = """
  <toolspecific tool="org.pnml.tool" version="1.0">
     <tokengraphics>
          <tokenposition x="-9" y="-2"/>
@@ -34,31 +30,60 @@ str4 = (tool="org.pnml.tool", version="1.0",
  </toolspecific>
 """)
 
-str5 = (tool="org.pnml.tool", version="1.0",
-        str = """
- <toolspecific tool="org.pnml.tool" version="1.0">
-    <tokengraphics>
-         <tokenposition x="-9" y="-2"/>
-         <tokenposition x="2"  y="3"/>
-     </tokengraphics>
-     <visible>true</visible>
- </toolspecific>
+#TODO! NUPN API
+# located either inside the "net" element or inside the (supposedly unique) "page" element of the PNML file.
+#
+str5 = (tool="nupn", version="1.1", str = """
+<toolspecific tool="nupn" version="1.1">
+    <size places="1" transitions="1" arcs="1"/>
+    <structure units="2" root="u0" safe="false"/>
+    <unit id="u0">
+        <places/>
+        <subunits>u1</subunits>
+    </unit>
+    <unit id="u1">
+        <places>p1</places>
+        <subunits/>
+    </unit>
+</toolspecific>
 """)
 
-@testset "parse tool $(s.tool) $(s.version)" for s in [str1, str2, str3, str4, str5]
-        tooli = parse_toolspecific(xmlroot(s.str), PnmlCoreNet())
+str6 = (tool="WoPeD", version="1.0", str = """
+<toolspecific version="1.0" tool="WoPeD">
+    <!-- there are empty toolinfos in the examples -->
+</toolspecific>
+""")
 
-        @test isa(tooli, ToolInfo)
-        @test name(tooli) == s.tool
-        @test PNML.Labels.version(tooli) == s.version
+str7 = (tool="WoPeD", version="1.0", str = """
+<toolspecific version="1.0" tool="WoPeD">
+    <trigger type="200" id="">
+        <graphics>
+            <position y="30" x="290" />
+            <dimension y="22" x="24" />
+        </graphics>
+    </trigger>
+    <operator type="105" id="t14" />
+    <time>0</time>
+    <timeUnit>1</timeUnit>
+</toolspecific>
+""")
 
-        @test get_toolinfo([tooli], s.tool, s.version) == tooli # Is identity on scalar
-        @test get_toolinfo([tooli], s.tool) == tooli
-        @test get_toolinfo([tooli], s.tool, r"^.*$") == tooli
-        @test get_toolinfo([tooli], Regex(s.tool), r"^.*$") == tooli
-        @test get_toolinfo([tooli], Regex(s.tool)) == tooli
+#-----------------------------------------------------------------------------------
+@testset "parse tool specific info $(s.tool) $(s.version)" for s in [str1, str2, str3, str4, str5]
+    println("\n###### parse tool $(s.tool) $(s.version)")
+    @show tooli = parse_toolspecific(xmlroot(s.str), PnmlCoreNet())
 
-        @test_call broken=false get_toolinfo([tooli], s.tool, s.version)
+    @test isa(tooli, ToolInfo)
+    @test name(tooli) == s.tool
+    @test PNML.Labels.version(tooli) == s.version
+
+    @test get_toolinfo([tooli], s.tool, s.version) == tooli # Is identity on scalar
+    @test get_toolinfo([tooli], s.tool) == tooli
+    @test get_toolinfo([tooli], s.tool, r"^.*$") == tooli
+    @test get_toolinfo([tooli], Regex(s.tool), r"^.*$") == tooli
+    @test get_toolinfo([tooli], Regex(s.tool)) == tooli
+
+    @test_call broken=false get_toolinfo([tooli], s.tool, s.version)
 end
 
 @testset "combined tools" begin
