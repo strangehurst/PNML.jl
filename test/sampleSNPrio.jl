b4b2955193032f90ff77d644bdc5df070ac62f6f
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-06-16 17:30:17 -0500

    XMLTestFilesRepository correct full_sn.xml

--- test/sampleSNPrio.jl
+++ test/sampleSNPrio.jl
@@ -2,11 +2,27 @@ using PNML, ..TestUtils, JET, OrderedCollections, AbstractTrees
 #
 # Read a SymmetricNet with partitions & tuples from pnmlframework test file.
 # NB: This model is from part 2 of the ISO 15909 standard as informative.
-#
+# From ePNK
 println("-----------------------------------------")
-println("NET2 sampleSNPrio.pnml")
+println("sampleSNPrio.pnml")
 println("-----------------------------------------\n"); flush(stdout)
 @testset let fname=joinpath(@__DIR__, "data", "sampleSNPrio.pnml")
+    #false &&
+    model = pnmlmodel(fname)::PnmlModel
+    # println("model = ", model) #! debug
+    #@test PNML.verify(net; verbose=true)
+    #TODO apply metagraph tools
+    println(); flush(stdout)
+end
+
+#
+# pnmlframework-2.2.16/pnmlFw-Tests/XMLTestFilesRepository/Oracle
+#
+datapath = "data/XMLTestFilesRepository/Oracle"
+println("-----------------------------------------")
+println("full_sn.xml")
+println("-----------------------------------------\n"); flush(stdout)
+@testset let fname=joinpath(@__DIR__, datapath, "full_sn.xml")
     #false &&
     model = pnmlmodel(fname)::PnmlModel
     println("model = ", model) #! debug
