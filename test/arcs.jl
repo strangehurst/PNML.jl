a2230d959facab4a155d175ace6443437b47173f
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-12-24 15:38:10 -0600

    restored arc tests with more scaffolding

--- test/arcs.jl
+++ test/arcs.jl
@@ -4,74 +4,90 @@ using PNML, ..TestUtils, JET, XMLDict
 # ARC
 #---------------------------------------------
 
-using PNML: isnormal, isinhibitor, isread, isreset
-
-@testset "arctypes $arct" for arct in ["normal", "inhibitor", "read", "reset"]
-    pntd = PnmlCoreNet()
-
-    str = """<arc source="t1" target="p1" id="a1">
-        <arctype>
-            <text> $arct </text>
-        </arctype>
-      </arc>"""
-    #@show str
-    node = xmlnode(str)
+println("\nARC\n")
+@testset "arc $pntd" for pntd in PnmlTypes.all_nettypes()
     PNML.CONFIG[].warn_on_unclaimed = true
     parse_context = PNML.Parser.parser_context()
+    netdata = PNML.PnmlNetData()
+    netsets = PNML.PnmlNetKeys()
 
-    a = parse_arc(node, pntd, netdata=PNML.PnmlNetData(); parse_context)::Arc
-    atl = PNML.arctypelabel(a)
-    arct = PNML.Labels.arctype(atl)
+    insc_xml = if ishighlevel(pntd)
+        """<hlinscription>
+            <text>6</text>
+            <structure>
+                <numberof>
+                    <subterm>
+                        <numberconstant value="6"> <positive/> </numberconstant>
+                    </subterm>
+                    <subterm> <dotconstant/> </subterm>
+                </numberof>
+            </structure>
+           </hlinscription>"""
+    else
+        """<inscription> <text>6</text> </inscription>"""
+    end
 
-    @test length(Base.findall([isnormal(a), isinhibitor(a), isread(a), isreset(a)])) == 1
-    @test length(Base.findall([isnormal(atl), isinhibitor(atl), isread(atl), isreset(atl)])) == 1
-    @test length(Base.findall([isnormal(arct), isinhibitor(arct), isread(arct), isreset(arct)])) == 1
+    #! arc needs :place1 for adjacent place
+    let pl_node = if ishighlevel(pntd)
+            xml"""
+                <place id="place1">
+                <name> <text>with text</text> </name>
+                <type><structure><dot/></structure></type>
+                <hlinitialMarking>
+                    <text>101</text>
+                    <structure>
+                        <numberof>
+                            <subterm>
+                                <numberconstant value="11"><positive/></numberconstant>
+                            </subterm>
+                            <subterm><dotconstant/></subterm>
+                        </numberof>
+                    </structure>
+                </hlinitialMarking>
+                </place>
+                """
+        else
+            xml"""
+                <place id="place1">
+                <initialMarking> <text>1</text> </initialMarking>
+                </place>
+                """
+        end
+        pl = parse_place(pl_node, pntd; parse_context)
+        push!(PNML.place_idset(netsets), pid(pl))
+        PNML.placedict(netdata)[pid(pl)] = pl
+    end
 
-    @test isnormal(a) == isnormal(atl) == isnormal(arct)
-    @test isinhibitor(a) == isinhibitor(atl) ==isinhibitor(arct)
-    @test isread(a) == isread(atl) == isread(arct)
+    #! arc needs :transition1 for adjacent transition
+    let tr_node = xml"""<transition id="transition1" />"""
+        tr = parse_transition(tr_node, pntd; parse_context)
+        push!(PNML.transition_idset(netsets), pid(tr))
+        PNML.transitiondict(netdata)[pid(tr)] = tr
+    end
 
-    @test pid(a) === :a1
-    @test !has_name(a)
-    @test inscription(a)(NamedTuple()) == 1
+    node = xmlnode("""
+      <arc source="transition1" target="place1" id="arc1">
+        <name> <text>Some arc</text> </name>
+        $insc_xml
+        <unknown id="unkn">
+            <name> <text>unknown label</text> </name>
+            <text>content text</text>
+        </unknown>
+      </arc>
+    """)
+
+    a = @test_logs(match_mode=:any,
+                  (:info, "add PnmlLabel :unknown to :arc1"),
+                  parse_arc(node, pntd; netdata, parse_context))
+    @test typeof(a) <: Arc
+    @test pid(a) === :arc1
+    @test has_name(a)
+    @test name(a) == "Some arc"
+    @test_call inscription(a)
+    #@show inscription(a)(NamedTuple())
+    if ishighlevel(pntd) # assumes storttype of dot
+        @test PNML.cardinality(inscription(a)(NamedTuple())) == 6
+    else
+        @test inscription(a)(NamedTuple()) == 6
+    end
 end
-
-#! Needs scaffolding
-# @testset "arc $pntd"  for pntd in PnmlTypes.all_nettypes()
-#     insc_xml = if ishighlevel(pntd)
-#         """<hlinscription>
-#             <text>6</text>
-#             <structure> 6 </structure>
-#            </hlinscription>"""
-#     else
-#         """<inscription> <text>6</text> </inscription>"""
-#     end
-
-#     node = xmlnode("""
-#       <arc source="transition1" target="place1" id="arc1">
-#         <name> <text>Some arc</text> </name>
-#         $insc_xml
-#         <unknown id="unkn">
-#             <name> <text>unknown label</text> </name>
-#             <text>content text</text>
-#         </unknown>
-#       </arc>
-#     """)
-#     PNML.CONFIG[].warn_on_unclaimed = true
-#     parse_context = PNML.Parser.parser_context()
-#     if ishighlevel(pntd)
-#         @test_throws("ArgumentError: missing inscription term in <structure>",
-#                     parse_arc(node, pntd, netdata=PNML.PnmlNetData(); parse_context))
-#     else
-#         a = @test_logs(match_mode=:any,
-#                 (:warn, "found unexpected child of <arc>: unknown"),
-#                 parse_arc(node, pntd, netdata=PNML.PnmlNetData(); parse_context))
-
-#         @test typeof(a) <: Arc
-#         @test pid(a) === :arc1
-#         @test has_name(a)
-#         @test name(a) == "Some arc"
-#         @test_call  inscription(a)
-#         @test inscription(a)(NamedTuple()) == 6
-#     end
-# end
