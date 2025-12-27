c733980beb2de367c6b58667147e56b905a34e84
Jeff Hurst <strangehurst@users.noreply.github.com>
2025-12-26 18:12:54 -0600

    remove has_labels(::AbstractLabel)

--- src/Labels/labels.jl
+++ src/Labels/labels.jl
@@ -74,8 +74,6 @@ graphics(l::AbstractLabel) =  l.graphics
 has_tools(l::AbstractLabel) = hasproperty(l, :toolspecinfos) && !isnothing(l.toolspecinfos)
 toolinfos(l::AbstractLabel) = l.toolspecinfos
 
-# has_labels(l::AbstractLabel) = false # Labels DO NOT have sub-labels.
-
 #--------------------------------------------
 
 """
