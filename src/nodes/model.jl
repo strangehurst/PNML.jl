d8efccfbbbc175a484144813fcf4e384e687f752
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-01-03 14:28:01 -0600

    replace === with isa

--- src/nodes/model.jl
+++ src/nodes/model.jl
@@ -19,12 +19,12 @@ namespace(model::PnmlModel) = model.namespace
 
 """
 $(TYPEDSIGNATURES)
-Return nets matching pntd `type` given as symbol or pnmltype singleton.
+Return nets matching pntd `type` given as string, symbol or pnmltype instance.
 """
 function find_nets end
 find_nets(model, str::AbstractString) = find_nets(model, PnmlTypes.pntd_symbol(str))
 find_nets(model, sym::Symbol)    = find_nets(model, pnmltype(sym))
-find_nets(model, pntd::PnmlType) = Iterators.filter(n -> Fix1(===, pntd)(nettype(n)), nets(model))
+find_nets(model, pntd::PnmlType) = Iterators.filter(n -> Fix1(isa, pntd)(nettype(n)), nets(model))
 
 """
 $(TYPEDSIGNATURES)
