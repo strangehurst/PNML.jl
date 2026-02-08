d66b0edc001c3e71fa528163d0b6182712ed2155
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-02-07 20:24:07 -0600

    ToolParse callable type parameter

--- src/Core/toolparser.jl
+++ src/Core/toolparser.jl
@@ -8,15 +8,13 @@ Holds a parser callable for a `<toolspecific>` tag's well-formed contents.
 Will be in an iteratable collection that maps tool name & version to a parser callable.
 See `toolspecific_content_fallback(node, pntd)`.
 """
-@auto_hash_equals struct ToolParser
+@auto_hash_equals struct ToolParser{T}
     toolname::String
     version::String
-    func::Base.Callable
+    func::T
 end
 
-"Name of tool."
 PNML.name(ti::ToolParser) = ti.toolname
-
 version(ti::ToolParser) = ti.version
 
 "Return callable parser of a ToolInfo."
