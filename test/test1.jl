19d8dcec5c89c448a772d7583e0157badb8fa4a0
Jeff Hurst <strangehurst@users.noreply.github.com>
2026-02-14 16:03:05 -0600

    less printing

--- test/test1.jl
+++ test/test1.jl
@@ -18,33 +18,28 @@ println("-----------------------------------------\n")
 #     #!@show model
 #     #~ repr tests everybody's show() methods. #! Errors exposed warrent test BEFORE HERE!
 #     #!@test startswith(repr(model), "PnmlModel")
-
-    @test all(n->pid(n) in Set([:net1,:net2,:net3,:net4,:net5,:net6,
-                                :net7,:net8,:net9,:net10,:net11]), PNML.nets(model))
-
-    for n in PNML.nets(model)
-        println("-----------------------------------------")
-        println(summary(n))
-        @test PNML.verify(n, false)
-        PNML.flatten_pages!(n; verbose=false)
-        @test PNML.verify(n, false)
-        #Base.redirect_stdio(stdout=devnull, stderr=devnull) do
-        Base.redirect_stdio(stdout=nothing, stderr=nothing) do #! debug
-            #TODO use MetaGraph as base of a validation tool
-            vc = PNML.vertex_codes(n)::AbstractDict
-            vl = PNML.vertex_labels(n)::AbstractDict
-            for a in arcs(n)
-                println("Edge ",
-                    vc[PNML.source(a)], " -> ",  vc[PNML.target(a)], " or ",
-                    vl[vc[PNML.source(a)]], " -> ",  vl[vc[PNML.target(a)]],
-                    )
-            end
-            if !(narcs(n) > 0 && nplaces(n) > 0 && ntransitions(n) > 0)
-                @test_throws ArgumentError PNML.metagraph(n)
-            else
-                @test contains(sprint(show, PNML.metagraph(n)),
-                    "Meta graph based on a Graphs.SimpleGraphs.SimpleDiGraph{Int64}")
-            end
+    for net in PNML.nets(model)
+        @test pid(net) in Set([:net1,:net2,:net3,:net4,:net5,:net6,
+                               :net7,:net8,:net9,:net10,:net11])
+        #println("-----------------------------------------")
+        #println(summary(net))
+        @test PNML.verify(net, false)
+        PNML.flatten_pages!(net; verbose=false)
+        @test PNML.verify(net, false)
+        vc = PNML.vertex_codes(net)::AbstractDict
+        vl = PNML.vertex_labels(net)::AbstractDict
+        for a in arcs(net)
+            @test vl[vc[PNML.source(a)]] == PNML.source(a)
+            @test vl[vc[PNML.target(a)]] == PNML.target(a)
+            # println("Edge ", vc[PNML.source(a)], " -> ", vc[PNML.target(a)], " or ",
+            #                  vl[vc[PNML.source(a)]], " -> ", vl[vc[PNML.target(a)]],
+            #     )
+        end
+        if !(narcs(net) > 0 && nplaces(net) > 0 && ntransitions(net) > 0)
+            @test_throws ArgumentError PNML.metagraph(net)
+        else
+            @test contains(sprint(show, PNML.metagraph(net)),
+                "Meta graph based on a Graphs.SimpleGraphs.SimpleDiGraph{Int64}")
         end
     end
 end
